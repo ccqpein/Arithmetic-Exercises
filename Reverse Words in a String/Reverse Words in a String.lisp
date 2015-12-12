0346a2a60f29f4cf90501a23aa49240f340e6f56
ccQpein <ccqpein@protonmail.com>
2015-12-12 17:52:25 -0500

    keep going

--- Reverse Words in a String/Reverse Words in a String.lisp
+++ Reverse Words in a String/Reverse Words in a String.lisp
@@ -3,18 +3,24 @@
     (setf charList (coerce str 'list))
     charList))
 
-(defun judgeChar (char word wordsList)
+(defun judgeChar (char word)
   (cond ((char/= char #\ ) 
-         (push char word))
+         (progn
+           (push char word)
+           (return-from judgeChar word)))
         ((char= char #\ )
-         (progn (push char word) (setf word (reverse word)) (push word wordsList)))))
+         (progn (push char word) (setf word (reverse word))
+                (return-from judgeChar word)))))
 
 (defun deleteSpacesBeginning (charList)
   (let ((charList2 charList))
     (loop for i in charList do
          (if (char= i #\ )
-             (setf charList2 (cdr charList2))
-             (return-from deleteSpacesBeginning charList2)))))
+             (setf charList2 (cdr charList2))))
+    (loop for i in (reverse charList) do
+         (if (char= i #\ )
+             (setf charList2 (butlast charList2))))
+    (return-from deleteSpacesBeginning charList2)))
 
 (defun pushCharToList (charList)
   (let ((wordsList)
@@ -23,3 +29,15 @@
     (dolist (char charList)
       (judgeChar char word wordsList))
     wordsList))
+
+(defun pushCharToList (charList)
+  "charlist cannot be empty"
+  (do* ((charList (deleteSpacesBeginning charList)
+                  (cdr charList))
+        (char (car charList)
+              (car charList))
+        (word char
+              (judgeChar char word))
+        (wordsList nil
+                   (if (and (char= char #\ ) (char= char nil))
+                       (push word wordsList)))
