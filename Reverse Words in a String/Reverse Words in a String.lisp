dacc4a98b8a9c8b895b100fdb2a8cc36d198068c
ccQpein <ccqpein@protonmail.com>
2015-12-13 17:53:08 -0500

    keep working

--- Reverse Words in a String/Reverse Words in a String.lisp
+++ Reverse Words in a String/Reverse Words in a String.lisp
@@ -4,40 +4,47 @@
     charList))
 
 (defun judgeChar (char word)
-  (cond ((char/= char #\ ) 
+  (cond ((char/= char #\Space)
          (progn
            (push char word)
            (return-from judgeChar word)))
-        ((char= char #\ )
+        ((char= char #\Space)
          (progn (push char word) (setf word (reverse word))
-                (return-from judgeChar word)))))
+                (return-from judgeChar word)))
+        ((eql char nil)
+         (progn (push #\Space word) (setf word (reverse word))
+                  (return-from judgeChar word)))))
 
 (defun deleteSpacesBeginning (charList)
   (let ((charList2 charList))
-    (loop for i in charList do
-         (if (char= i #\ )
-             (setf charList2 (cdr charList2))))
-    (loop for i in (reverse charList) do
-         (if (char= i #\ )
-             (setf charList2 (butlast charList2))))
+    (loop for i in charList
+       until (char/= i #\Space )
+       do
+         (setf charList2 (cdr charList2)))
+    (loop for i in (reverse charList)
+       until (char/= i #\Space )
+       do
+         (setf charList2 (butlast charList2)))
     (return-from deleteSpacesBeginning charList2)))
 
-(defun pushCharToList (charList)
-  (let ((wordsList)
-        (word)
-        (charList (deleteSpacesBeginning charList)))
-    (dolist (char charList)
-      (judgeChar char word wordsList))
-    wordsList))
+(defun changeWordslistToString (wordsList)
+  (return-from changeWordslistToString wordsList))
+;  (let ((FinalString))
+;    (loop for word in wordsList do
+       
 
 (defun pushCharToList (charList)
   "charlist cannot be empty"
-  (do* ((charList (deleteSpacesBeginning charList)
+  (do* ((charList (deleteSpacesBeginning (changeStringtoChar charList))
                   (cdr charList))
         (char (car charList)
               (car charList))
-        (word char
+        (word (list char)
               (judgeChar char word))
-        (wordsList nil
-                   (if (and (char= char #\ ) (char= char nil))
-                       (push word wordsList)))
+        (wordsList (list )
+                   (if (or (char= char #\Space) (eql char nil))
+                       (push word wordsList))))
+       ((eql char nil) (let ((FinalString))
+                           (setf FinalString (changeWordslistToString wordsList))
+                           (return-from pushCharToList FinalString)))))
+;;;;The value NIL is not of type CHARACTER.
