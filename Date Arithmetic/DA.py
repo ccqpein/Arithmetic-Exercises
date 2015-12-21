6b88b68d7f6d279f3f9da88324fc5fac42a4387e
ccQpein <ccqpein@protonmail.com>
2015-12-20 22:40:28 -0500

    finish python version

--- Date Arithmetic/DA.py
+++ Date Arithmetic/DA.py
@@ -7,11 +7,31 @@ monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 def DateArithmetic(date, days):
     dateSeparete = []
     dd = ''
-    for i in str(date):
+    for i in date:
         try:
             ii = int(i)
             dd += i
         except ValueError:
             dateSeparete.append(int(dd))
+            dd = ''
             continue
-    print(dateSeparete, days)
+    dateSeparete.append(int(dd))
+
+    while(days):
+        if dateSeparete[2] % 4 == 0:
+            monthDays[1] = 29
+        else:
+            monthDays[1] = 28
+        if days > (monthDays[dateSeparete[0] - 1] - dateSeparete[1]):
+            days -= monthDays[dateSeparete[0] - 1] - dateSeparete[1]
+            dateSeparete[0] += 1
+            dateSeparete[1] = 0
+            if dateSeparete[0] == 13:
+                dateSeparete[0] = 1
+                dateSeparete[2] += 1
+            continue
+        else:
+            dateSeparete[1] += days
+            days = 0
+
+    print(dateSeparete)
