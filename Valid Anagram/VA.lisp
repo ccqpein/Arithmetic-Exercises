92493da8165de746fb2091c3cd481ac60b75cbf5
ccQpein <ccqpein@protonmail.com>
2015-12-24 00:42:45 -0500

    finish lisp version

--- Valid Anagram/VA.lisp
+++ Valid Anagram/VA.lisp
@@ -1,4 +1,10 @@
 (defun isAnagram (s tt)
-    (do* ((ss (coerce s 'list) (cdr ss))
-          (ttt (reverse (coerce tt 'list)) (cdr ttt)))
-         ((not (eql (car ss) (car ttt))) (format t "succes"))))
+  (let ((ss (coerce s 'list))
+        (ttt (reverse (coerce tt 'list))))
+    (print ss)
+    (print ttt)
+    (return-from isAnagram (listpp ss ttt))))
+
+(defun listpp (list1 list2)
+  (cond ((equal list1 list2) (return-from listpp 'true))
+        (t (return-from listpp 'false))))
