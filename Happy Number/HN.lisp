75183dcf5b7031495e59650d33acaf520d9ee69a
ccQpein <ccqpein@protonmail.com>
2015-12-25 21:37:16 -0500

    add new && finish lisp version

---
+++ Happy Number/HN.lisp
@@ -0,0 +1,22 @@
+(defun splitNumber (num)
+  (let* ((numStr (write-to-string num))
+         (numList (mapcar #'digit-char-p (coerce numStr 'list))))
+    numList))
+
+(defun numReturn (numList)
+  (let ((sumNum
+    (apply #'+ 
+           (mapcar #'(lambda (x)
+                       (* x x))
+                   numList))))
+    sumNum))
+
+(defun isHappy (num)
+  (let* ((numList (splitNumber num))
+         (isHappyy (numReturn numList)))
+    (if (= (length numList) 1)
+        (if (= isHappyy 1)
+            (return-from isHappy 'true)
+            (return-from isHappy 'nil))
+        (isHappy isHappyy))))
+        
