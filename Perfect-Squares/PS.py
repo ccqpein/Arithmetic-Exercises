b8c7cef8b58dfc6454a3ecc61b9ec48ee78f3265
ccQpein <ccqpein@protonmail.com>
2015-12-30 00:41:52 -0500

    finish python version but no efficient

--- Perfect-Squares/PS.py
+++ Perfect-Squares/PS.py
@@ -1,16 +1,24 @@
 def numSquares(n):
-    squaresSumList = [i * i for i in xrange(n)]
+    squaresSumList = [i * i for i in xrange(n + 1)]
     squaresSumList2 = []
-    rTime = 0
     for summ in squaresSumList:
         if summ <= n:
-            squaresSumList2.append(sum)
-            continue
+            squaresSumList2.append(summ)
     squaresSumList = squaresSumList2
-    squaresSumList2.reverse()
-    for summ2 in squaresSumList2:
-        rTime = 2
+    for i in xrange(1, 10):
+        if n in geneTable(i, squaresSumList):
+            return True, i
+    return "something wrong"
+
+
+def geneTable(n, squaresSumList):
+    squaresSumList2 = squaresSumList
+    while(n - 1):
+        squaresSumList3 = []
         for summ in squaresSumList:
-            xx = summ + summ2
-            if xx == n:
-                return rTime
+            for summ2 in squaresSumList2:
+                squaresSumList3.append(summ + summ2)
+        squaresSumList2 = squaresSumList3
+        n -= 1
+#    print(squaresSumList2)
+    return squaresSumList2
