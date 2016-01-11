4403172ec92ce64760b2dc72fedd6d256bc4e08c
ccQpein <ccqpein@protonmail.com>
2016-01-11 14:01:36 -0500

    finish the first go version; todo:shell avar input

--- Move-Zeroes/MZ.go
+++ Move-Zeroes/MZ.go
@@ -5,11 +5,21 @@ import (
 	"os"
 )
 
-var nums []int
-
-func main() {
-	nums := []int{1, 2, 3}
+func MoveZeros(nums ...int) []int {
+	//nums := []int{1, 2, 3}
+	x := 1
 	for i := 0; i < len(nums); i++ {
-		fmt.Print(nums[i])
+		if nums[x] == 0 {
+			nums = append(nums[:x], nums[x+1:len(nums)]...)
+			nums = append(nums, 0)
+		} else {
+			x++
+		}
+
 	}
+	fmt.Print(nums)
+	return nums
+}
+func main() {
+	MoveZeros(os.Args[1:])
 }
