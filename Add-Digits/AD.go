3049e4c71dd4483bdf054b9f468f06d3ece98533
ccQpein <ccqpein@protonmail.com>
2016-01-16 11:33:36 -0500

    finish go version

--- Add-Digits/AD.go
+++ Add-Digits/AD.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 )
 
-func AddDigit(numStr str) str{
-	for _,s := range numStr {
-		
+var args []string = os.Args[1:]
+
+func AddDigit(numStr string) string {
+	numSum := 0
+	var ss string
+	for _, s := range numStr {
+		numSum = numSum + int(s-'0')
+	}
+
+	if numSum < 10 {
+		ss = strconv.Itoa(numSum)
+	} else {
+		ss = AddDigit(strconv.Itoa(numSum))
+	}
+	return ss
 }
 
 func main() {
-	fmt.Println("new one")
+	fmt.Println(AddDigit(args[0]))
 }
