c289cbf474434328680afe4b2c6ae9eb84555058
ccQpein <ccqpein@protonmail.com>
2016-01-19 14:08:50 -0500

    find a new way to split string

--- Reverse-Words-in-a-String/RWIAS.go
+++ Reverse-Words-in-a-String/RWIAS.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func ReverseWords(stringR string) []string {
-	indexSpace = make([]int, 0)
+	//indexSpace = make([]int, 0)
 	var wordsList []string
-
+	/**
 	for i, s := range stringR {
 		if s == " " {
 			indexSpace = append(indexSpace, i)
@@ -17,5 +18,12 @@ func ReverseWords(stringR string) []string {
 		if i < len(indexSpace)-1; indexSpace[i+1]-indexSpace[i] >= 2 {
 			wordsList = append(wordsList, stringR[indexSpace[i]:indexSpace[i+1]])
 		}
-	}
+	}*/
+
+	wordsList = strings.Split(stringR, " ")
+	return wordsList
+}
+
+func main() {
+	fmt.Print(ReverseWords("ccQ nihao"))
 }
