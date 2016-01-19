f3f795b25461cf1c5f301c59b2efee6f1ee36232
ccQpein <ccqpein@protonmail.com>
2016-01-18 23:03:23 -0500

    still working

--- Reverse-Words-in-a-String/RWIAS.go
+++ Reverse-Words-in-a-String/RWIAS.go
@@ -5,12 +5,17 @@ import (
 )
 
 func ReverseWords(stringR string) []string {
-	var indexSpace []int
+	indexSpace = make([]int, 0)
 	var wordsList []string
+
 	for i, s := range stringR {
 		if s == " " {
 			indexSpace = append(indexSpace, i)
 		}
 	}
-
+	for i, _ := range indexSpace {
+		if i < len(indexSpace)-1; indexSpace[i+1]-indexSpace[i] >= 2 {
+			wordsList = append(wordsList, stringR[indexSpace[i]:indexSpace[i+1]])
+		}
+	}
 }
