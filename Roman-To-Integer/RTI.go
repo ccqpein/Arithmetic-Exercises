0894eded1692efec36640d1bd3c6f9790aa171a6
ccQpein <ccqpein@protonmail.com>
2016-01-22 23:14:16 -0500

    finish go version

--- Roman-To-Integer/RTI.go
+++ Roman-To-Integer/RTI.go
@@ -2,18 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
-var dicOfLetters = make(map[string]int)
-
-dicOfLetters["I"]=1
-dicOfLetters["V"]=5
-dicOfLetters["X"]=10
-dicOfLetters["D"]=500
-dicOfLetters["L"]=50
-dicOfLetters["C"]=100
-dicOfLetters["M"]=1000
-
+var args []string = os.Args[1:]
 
 func splitString(s string) []string {
 	var returnArray []string
@@ -24,17 +16,6 @@ func splitString(s string) []string {
 }
 
 func RomanToInt(s string) int {
-	charList := splitString(s)
-	sumNumber := 0
-	char2, char1 := 0, 0
-	for i := 0; i < len(charList)-1; i++ {
-		char2 = dicOfLetters[charList[i]]
-		fmt.Println(char2)
-	}
-
-}
-
-func main() {
 	dicOfLetters := map[string]int{
 		"I": 1,
 		"V": 5,
@@ -44,7 +25,27 @@ func main() {
 		"C": 100,
 		"M": 1000,
 	}
-	fmt.Println(dicOfLetters)
-	fmt.Println(splitString("XIIX"))
-	fmt.Println(RomanToInt("XIIX"))
+
+	charList := splitString(s)
+	sumNumber := 0
+	char2, char1 := 0, 0
+	for i := 0; i < len(charList); i++ {
+		char2 = dicOfLetters[charList[i]]
+		sumNumber = sumNumber + char2
+		switch {
+		case (char2 == 5 || char2 == 10) && char1 == 1:
+			sumNumber = sumNumber - 2
+		case (char2 == 1000 || char2 == 500) && char1 == 100:
+			sumNumber = sumNumber - 200
+		case (char2 == 100 || char2 == 50) && char1 == 10:
+			sumNumber = sumNumber - 20
+		}
+		char1 = char2
+	}
+	return sumNumber
+}
+
+func main() {
+	//fmt.Println(splitString("XIIX"))
+	fmt.Println(RomanToInt(args[0]))
 }
