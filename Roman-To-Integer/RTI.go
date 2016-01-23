02782f843e89948ae8055ca2d0123bb61e36a2a0
ccQpein <ccqpein@protonmail.com>
2016-01-22 22:42:36 -0500

    keep doing

--- Roman-To-Integer/RTI.go
+++ Roman-To-Integer/RTI.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 )
 
+var dicOfLetters = make(map[string]int)
+
+dicOfLetters["I"]=1
+dicOfLetters["V"]=5
+dicOfLetters["X"]=10
+dicOfLetters["D"]=500
+dicOfLetters["L"]=50
+dicOfLetters["C"]=100
+dicOfLetters["M"]=1000
+
+
 func splitString(s string) []string {
 	var returnArray []string
 	for i := 0; i < len(s); i++ {
@@ -15,8 +26,10 @@ func splitString(s string) []string {
 func RomanToInt(s string) int {
 	charList := splitString(s)
 	sumNumber := 0
+	char2, char1 := 0, 0
 	for i := 0; i < len(charList)-1; i++ {
-
+		char2 = dicOfLetters[charList[i]]
+		fmt.Println(char2)
 	}
 
 }
