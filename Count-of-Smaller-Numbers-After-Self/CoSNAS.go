8ed0b85d5dce50d1cccb2963f2a0c60f583146a9
ccQpein <ccqpein@protonmail.com>
2016-01-28 13:47:46 -0500

    finish go version

--- Count-of-Smaller-Numbers-After-Self/CoSNAS.go
+++ Count-of-Smaller-Numbers-After-Self/CoSNAS.go
@@ -4,6 +4,24 @@ import (
 	"fmt"
 )
 
+func countSmaller(nums []int) []int {
+	var nums2 []int
+
+	for i, numb := range nums {
+		timeCount := 0
+		for ii := i; ii < len(nums); ii++ {
+			if nums[ii] < numb {
+				timeCount = timeCount + 1
+			}
+		}
+		nums2 = append(nums2, timeCount)
+	}
+
+	return nums2
+
+}
+
 func main() {
-	fmt.Println("new version")
+	args := []int{5, 2, 6, 1}
+	fmt.Println(countSmaller(args))
 }
