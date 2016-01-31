fe17d8531ae2896975ee254d09f4f3578b68b33b
ccQpein <ccqpein@protonmail.com>
2016-01-30 23:34:21 -0500

    add two dimension array

--- Perfect-Squares/PS.go
+++ Perfect-Squares/PS.go
@@ -6,15 +6,24 @@ import (
 
 type numInput int
 
-func (num numInput) GeneTabel() []int {
+func (num numInput) GeneTabel() [][]int {
 	var squaresList []int
-	for i := 1; i < num; i++ {
+	for i := 1; i < int(num); i++ {
 		squaresList = append(squaresList, i*i)
 	}
-	return squaresList
+
+	var squaresTable [][]int
+	for r, s := range squaresList {
+		for c, ss := range squaresList {
+			squaresTable[r][c] = s + ss
+		}
+	}
+	return squaresTable
 
 }
 
 func main() {
+	var nn numInput = 5
 	fmt.Println("new version")
+	fmt.Println(nn.GeneTabel())
 }
