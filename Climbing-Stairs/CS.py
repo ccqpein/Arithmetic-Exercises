eba2cb136e2b94e24aa3d83d0de6c3033fb83afa
ccQpein <ccqpein@protonmail.com>
2016-02-09 14:25:54 -0500

    im back

--- Climbing-Stairs/CS.py
+++ Climbing-Stairs/CS.py
@@ -1,9 +1,22 @@
 from math import factorial
 
 
+def genList(n):
+    resultList = []
+    for i in xrange(n):
+        ii = i + 1
+        if (n - ii) % 2 == 0:
+            resultList.append((ii, (n - ii) / 2))
+    print(resultList)
+    return resultList
+
+
 def cNquM(n, m):
     nn = factorial(n)
     nn = nn / factorial(n - m)
     mm = factorial(m)
-
     return nn / mm
+
+
+def main():
+    print("ddd")
