1d74e42832f387e039ada4cfda54cee51cc7a8bd
ccQpein <ccqpein@protonmail.com>
2016-02-12 20:19:31 -0500

    still coding lisp version

--- Climbing-Stairs/CS.lisp
+++ Climbing-Stairs/CS.lisp
@@ -1,8 +1,9 @@
 (defpackage :Climbing-Stairs
   (:use :cl)
-  (:export))
+  (:export :factorial))
 
 (defun factorial (n &optional m &key (c 'nil))
+  "C n qu m"
   (let ((result 1)
         (times (if m
                    m
@@ -14,4 +15,17 @@
       (setf result (* result n))
       (setf n (- n 1))
       (setf cOption (* cOption (+ i 1))))))
-    
+
+(defun genList (n)
+  (let ((resultList '()))
+    (loop for i from 0 to n
+       do (if (= (mod (- n i) 2) 0)
+              (setf resultList
+                    (append resultList (list (list i (/ (- n i) 2)))))))
+    (return-from genList resultList)))
+
+(defun CS (n)
+  (let ((resultList (genList n))
+        (allWaysSum 0))
+    (loop for i in resultList
+         do (factorial (sum (first     
