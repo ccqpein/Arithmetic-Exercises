366d517c75f2551d02050739c63b36cf9a963c26
ccQpein <ccqpein@protonmail.com>
2016-02-21 01:05:15 -0500

    finish part of

--- Longest-Increasing-Path-in-a-Matrix/LIPiaM.py
+++ Longest-Increasing-Path-in-a-Matrix/LIPiaM.py
@@ -1,3 +1,4 @@
+"""
 def changeDataToList(matrix):
     returnList, returnList2 = [], []
     for i in matrix:
@@ -8,7 +9,7 @@ def changeDataToList(matrix):
         if i not in returnList2:
             returnList2.append(i)
     returnList = returnList2
-    return returnList
+    return returnList"""
 
 
 def findAround(matrix, row, col):
@@ -18,26 +19,35 @@ def findAround(matrix, row, col):
     except:
         return False
 
-
+"""
 def pointMatrix(twoDimMatrix, rowNum, colNum):
     def _first(func):
         def _sencond():
             func
         return _sencond
-    return _first
+    return _first"""
 
 
 def genList(matrix, row, col, parentList):
+    returnDict = {}
+
     for changeNum in [-1, 1]:
         if findAround(matrix, row + changeNum, col) and \
            matrix[row][col] < matrix[row + changeNum][col]:
             print(matrix[row + changeNum][col])
+            pathList = parentList
+            print(pathList + [matrix[row + changeNum][col]])
+            returnDict[(row + changeNum, col)
+                       ] = pathList + [matrix[row + changeNum][col]]
         if findAround(matrix, row, col + changeNum) and \
            matrix[row][col] < matrix[row][col + changeNum]:
             print(matrix[row][col + changeNum])
+            pathList = parentList
+            print(pathList + [matrix[row][col + changeNum]])
+            returnDict[(row, col + changeNum)
+                       ] = pathList + [matrix[row][col + changeNum]]
+    return returnDict
 
 
 def main(m):
-
-    sortedList = changeDataToList(m)
-    print(startIndex)
+    pass
