97c8f154a8ab4b15ead27b3d8d1feae03cd14ad3
ccQpein <ccqpein@protonmail.com>
2016-02-22 17:04:56 -0500

    I think I make question complex

--- Longest-Increasing-Path-in-a-Matrix/LIPiaM.py
+++ Longest-Increasing-Path-in-a-Matrix/LIPiaM.py
@@ -19,13 +19,13 @@ def findAround(matrix, row, col):
     except:
         return False
 
-"""
-def pointMatrix(twoDimMatrix, rowNum, colNum):
+
+def pointMatrix(twoDimMatrix):
     def _first(func):
         def _sencond():
             func
         return _sencond
-    return _first"""
+    return _first
 
 
 def genList(matrix, row, col, parentList):
@@ -34,19 +34,36 @@ def genList(matrix, row, col, parentList):
     for changeNum in [-1, 1]:
         if findAround(matrix, row + changeNum, col) and \
            matrix[row][col] < matrix[row + changeNum][col]:
-            print(matrix[row + changeNum][col])
+            # print(matrix[row + changeNum][col])
             pathList = parentList
-            print(pathList + [matrix[row + changeNum][col]])
+            # print(pathList + [matrix[row + changeNum][col]])
             returnDict[(row + changeNum, col)
                        ] = pathList + [matrix[row + changeNum][col]]
         if findAround(matrix, row, col + changeNum) and \
            matrix[row][col] < matrix[row][col + changeNum]:
-            print(matrix[row][col + changeNum])
+            # print(matrix[row][col + changeNum])
             pathList = parentList
-            print(pathList + [matrix[row][col + changeNum]])
+            # print(pathList + [matrix[row][col + changeNum]])
             returnDict[(row, col + changeNum)
                        ] = pathList + [matrix[row][col + changeNum]]
+    print(returnDict)
     return returnDict
+
+
+def merge_two_dicts(x, y):
+    '''Given two dicts, merge them into a new dict as a shallow copy.'''
+    z = x.copy()
+    z.update(y)
+    return z
+
+
+def finishList(matrix, returnDict):
+    initDict = {}
+    for key, value in returnDict.items():
+        print(key)
+        initDict = merge_two_dicts(
+            genList(matrix, key[0], key[1], value), initDict)
+    return initDict
 
 
 def main(m):
