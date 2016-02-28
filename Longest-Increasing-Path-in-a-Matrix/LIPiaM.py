36937bd3124b8d7ec3da725ec68373244dc46697
ccQpein <ccqpein@protonmail.com>
2016-02-27 20:47:28 -0500

    none

--- Longest-Increasing-Path-in-a-Matrix/LIPiaM.py
+++ Longest-Increasing-Path-in-a-Matrix/LIPiaM.py
@@ -1,70 +1,20 @@
-"""
-def changeDataToList(matrix):
-    returnList, returnList2 = [], []
-    for i in matrix:
-        for ii in i:
-            returnList.append(ii)
-    returnList = sorted(returnList)
-    for i in returnList:
-        if i not in returnList2:
-            returnList2.append(i)
-    returnList = returnList2
-    return returnList"""
-
-
-def findAround(matrix, row, col):
+def findEleP(matrix, row, col):
     try:
         if matrix[row][col]:
-            return True
+            return True, matrix[row][col]
     except:
         return False
 
 
-def pointMatrix(twoDimMatrix):
-    def _first(func):
-        def _sencond():
-            func
-        return _sencond
-    return _first
+def recordNum(matrix, *coodNum):
+    row = coodNum[0]
+    col = coodNum[1]
+    reNum = 0
 
-
-def genList(matrix, row, col, parentList):
-    returnDict = {}
-
-    for changeNum in [-1, 1]:
-        if findAround(matrix, row + changeNum, col) and \
-           matrix[row][col] < matrix[row + changeNum][col]:
-            # print(matrix[row + changeNum][col])
-            pathList = parentList
-            # print(pathList + [matrix[row + changeNum][col]])
-            returnDict[(row + changeNum, col)
-                       ] = pathList + [matrix[row + changeNum][col]]
-        if findAround(matrix, row, col + changeNum) and \
-           matrix[row][col] < matrix[row][col + changeNum]:
-            # print(matrix[row][col + changeNum])
-            pathList = parentList
-            # print(pathList + [matrix[row][col + changeNum]])
-            returnDict[(row, col + changeNum)
-                       ] = pathList + [matrix[row][col + changeNum]]
-    print(returnDict)
-    return returnDict
-
-
-def merge_two_dicts(x, y):
-    '''Given two dicts, merge them into a new dict as a shallow copy.'''
-    z = x.copy()
-    z.update(y)
-    return z
-
-
-def finishList(matrix, returnDict):
-    initDict = {}
-    for key, value in returnDict.items():
-        print(key)
-        initDict = merge_two_dicts(
-            genList(matrix, key[0], key[1], value), initDict)
-    return initDict
-
-
-def main(m):
-    pass
+    for i in [-1, 1]:
+        rr = findEleP(matrix, row + i, col)
+        if rr and rr[1] > matrix[row][col]:
+            reNum += 1
+        rr2 = findEleP(matrix, row, col + i)
+        if rr2 and rr2[1] > matrix[row][col]:
+            reNum += 1
