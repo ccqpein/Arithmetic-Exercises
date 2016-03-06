d5583187cda1b213a575271caf40a2a9faf6b55e
ccQpein <ccqpein@protonmail.com>
2016-03-06 16:32:22 -0500

    done the main function,todo: make loop all elementries in maxtrix and read the data from the JSON

--- Longest-Increasing-Path-in-a-Matrix/LIPiaM.py
+++ Longest-Increasing-Path-in-a-Matrix/LIPiaM.py
@@ -44,7 +44,48 @@ def makeDic(matrix):
             col += 1
         row += 1
 #        print(row, col)
-    print(indexDic)
+#    print(indexDic)
+    return indexDic
+
+
+def findLongest(matrix, indexDic, row, col):
+    if indexDic[(row, col)][0] == 0:
+        return 1
+    elif indexDic[(row, col)][0] == 1:
+        value = 1 + findLongest(matrix, indexDic,
+                                indexDic[(row, col)][1][0][0],
+                                indexDic[(row, col)][1][0][1])
+        return value
+    elif indexDic[(row, col)][0] == 2:
+        value1 = 1 + findLongest(matrix, indexDic,
+                                 indexDic[(row, col)][1][0][0],
+                                 indexDic[(row, col)][1][0][1])
+        value2 = 1 + findLongest(matrix, indexDic,
+                                 indexDic[(row, col)][1][1][0],
+                                 indexDic[(row, col)][1][1][1])
+        if value1 >= value2:
+            return value1
+        else:
+            return value2
+
+    elif indexDic[(row, col)][0] == 3:
+        value1 = 1 + findLongest(matrix, indexDic,
+                                 indexDic[(row, col)][1][0][0],
+                                 indexDic[(row, col)][1][0][1])
+        value2 = 1 + findLongest(matrix, indexDic,
+                                 indexDic[(row, col)][1][1][0],
+                                 indexDic[(row, col)][1][1][1])
+        value3 = 1 + findLongest(matrix, indexDic,
+                                 indexDic[(row, col)][1][2][0],
+                                 indexDic[(row, col)][1][2][1])
+        return max(value1, value2, value3)
+
 
 if __name__ == "__main__":
-    makeDic([[9, 9, 4], [6, 6, 8], [2, 1, 1]])
+    matrix = [[9, 9, 4], [6, 6, 8], [2, 1, 1]]
+    indexDic = makeDic(matrix)
+    print(findLongest(matrix, indexDic, 2, 1))
+
+    matrix = [[3, 4, 5], [3, 2, 6], [2, 2, 1]]
+    indexDic = makeDic(matrix)
+    print(findLongest(matrix, indexDic, 0, 1))
