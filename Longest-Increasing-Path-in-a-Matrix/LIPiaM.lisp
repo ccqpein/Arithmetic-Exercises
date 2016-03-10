99f1068538a701637d68114335e4a2228dddcdc5
ccQpein <ccqpein@protonmail.com>
2016-03-09 23:00:53 -0500

    need fix the hash key type issue

--- Longest-Increasing-Path-in-a-Matrix/LIPiaM.lisp
+++ Longest-Increasing-Path-in-a-Matrix/LIPiaM.lisp
@@ -1,22 +1,24 @@
 (defvar *matrix* '((9 9 4) (6 6 8) (2 1 1)))
 
 (defun find-Ele (matrix row col)
-  (let ((ele (nth col (nth row matrix))))
-  (if ele
-      (return-from find-Ele (list ele (list row col) T))
-      (return-from find-Ele nil))))
+  (let ((ele))
+    (if  (and (>= row 0) (>= col 0))
+         (progn (setf ele  (nth col (nth row matrix)))
+                (if ele
+                    (return-from find-Ele (list ele (list row col) T)))
+                (return-from find-Ele nil)))))
 
 (defun do-around (func matrix row col)
   (let ((rowlist (list (+ 1 row) (- 1 row)))
         (collist (list (+ 1 col) (- 1 col)))
         (result))
-    (setf result
+    (setf result (remove 'nil
           (list
            (funcall func matrix row col)
            (funcall func matrix (nth 0 rowlist) col)
            (funcall func matrix (nth 1 rowlist) col)
            (funcall func matrix row (nth 0 collist))
-           (funcall func matrix row (nth 1 collist))))
+           (funcall func matrix row (nth 1 collist)))))
     result))
 
 (defun how-many-larger (resultFromDoAround)
@@ -31,3 +33,14 @@
                 (setf largerIndex (append largerIndex (list (cadr i))))
           )))))
 
+(defun main (matrix)
+  (let ((rowNum (length matrix))
+        (colNum (length (car matrix)))
+        (indexHash (make-hash-table)))
+    (loop for r from 0 to (1- rowNum) do
+       (loop for c from 0 to (1- colNum) do
+            (progn (print (list r c))
+              (setf (gethash (list r c) indexHash)
+                (how-many-larger (do-around 'find-ele *matrix* r c)))
+         (print (gethash (list r c) indexHash)))))
+    (print indexHash)))
