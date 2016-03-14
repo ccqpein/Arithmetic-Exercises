4ade8eb925a6906153983736e104ba553f49a158
ccQpein <ccqpein@protonmail.com>
2016-03-14 17:07:23 -0400

    finish the lisp version && dont wanna code go version

--- Longest-Increasing-Path-in-a-Matrix/LIPiaM.lisp
+++ Longest-Increasing-Path-in-a-Matrix/LIPiaM.lisp
@@ -1,4 +1,5 @@
 (defvar *matrix* '((9 9 4) (6 6 8) (2 1 1)))
+(defvar *matrix2* '((3 4 5) (3 2 2) (2 2 1)))
 
 (defun find-Ele (matrix row col)
   (let ((ele))
@@ -29,7 +30,8 @@
         (largerIndex '()))
     (dolist (i lastList (list largerNum largerIndex))
       (if (> (car i) thisNum)
-          (progn (incf largerNum)
+          (progn ;(print i)
+            (incf largerNum)
                 (setf largerIndex (append largerIndex (list (cadr i))))
           )))))
 
@@ -45,7 +47,9 @@
        (loop for c from 0 to (1- colNum) do
             (progn ;(print (change-index-to-string r c))
               (setf (gethash (change-index-to-string r c) indexHash)
-                (how-many-larger (do-around 'find-ele *matrix* r c))))))
+                    (how-many-larger (do-around 'find-ele matrix r c)))
+              ;(print (gethash (change-index-to-string r c) indexHash))
+              )))
     indexHash))
 
 (defun sum-list (list)
@@ -53,43 +57,55 @@
     (dolist (i list summation)
       (setf summation (+ i summation)))))
 
-(defun longest-list (&rest llist)
-  (let ((length-list '()))
-    (loop for i in llist do
-         (setf length-list (append length-list (length i))))
-    (max 
+(defun max-list (list)
+    (loop for element in list maximizing element))
 
-         
-  
+(defun longest-list ( &rest llist)
+  (let ((length-list '()))
+    ;(print llist)
+    (loop for i in llist do (progn
+                              ;(print i)
+                              (setf length-list (append length-list (list (length i))))
+                              ;(print length-list)
+                              ))
+    (let ((largerNum (max-list length-list)))
+      (loop for i from 0 to (1- (length length-list)) do
+           (if (= largerNum (nth i length-list))
+               (return-from longest-list (nth i llist)))))))
 
 (defun tree-search-matrix (indexTable r c)
   (let ((this-hash (gethash (change-index-to-string r c) indexTable))
         (result))
-  (cond ((= 0 (car this-hash))
-         (return '(1) ))
-        ((= 1 (car this-hash))
-         (return (setf result
+    ;(print (nth 1 this-hash))
+  (cond ((= 0 (car this-hash)) (progn ;(print (list r c))
+         (return-from tree-search-matrix '(1) )))
+        ((= 1 (car this-hash)) (progn ;(print (list r c))
+         (return-from tree-search-matrix (setf result
                        (append '(1) (tree-search-matrix indexTable
-                                                        (nth 1 (nth 1 (this-hash)))
-                                                        (nth 2 (nth 1 (this-hash))))))))
-        ((= 2 (car this-hash))
-         (return (setf result (longest-list 
+                                                        (nth 0 (nth 0 (nth 1 this-hash)))
+                                                        (nth 1 (nth 0 (nth 1 this-hash)))))))))
+        ((= 2 (car this-hash)) (progn ;(print (list r c))
+         (return-from tree-search-matrix (setf result (longest-list 
                        (append '(1) (tree-search-matrix indexTable
-                                                        (nth 1 (nth 1 (this-hash)))
-                                                        (nth 2 (nth 1 (this-hash)))))
+                                                        (nth 0 (nth 0 (nth 1 this-hash)))
+                                                        (nth 1 (nth 0 (nth 1 this-hash)))))
                        (append '(1) (tree-search-matrix indexTable
-                                                        (nth 1 (nth 2 (this-hash)))
-                                                        (nth 2 (nth 2 (this-hash)))))))))
-        ((= 2 (car this-hash))
-         (return (setf result (longest-list 
+                                                        (nth 0 (nth 1 (nth 1 this-hash)))
+                                                        (nth 1 (nth 1 (nth 1 this-hash))))))))))
+        ((= 3 (car this-hash)) (progn ;(print (list r c))
+         (return-from tree-search-matrix (setf result (longest-list 
                        (append '(1) (tree-search-matrix indexTable
-                                                        (nth 1 (nth 1 (this-hash)))
-                                                        (nth 2 (nth 1 (this-hash)))))
+                                                        (nth 0 (nth 0 (nth 1 this-hash)))
+                                                        (nth 1 (nth 0 (nth 1 this-hash)))))
                        (append '(1) (tree-search-matrix indexTable
-                                                        (nth 1 (nth 2 (this-hash)))
-                                                        (nth 2 (nth 2 (this-hash)))))))
+                                                        (nth 0 (nth 1 (nth 1 this-hash)))
+                                                        (nth 1 (nth 1 (nth 1 this-hash)))))
+                       (append '(1) (tree-search-matrix indexTable
+                                                        (nth 0 (nth 2 (nth 1 this-hash)))
+                                                        (nth 1 (nth 2 (nth 1 this-hash)))))))))))
+  result))
         
-#|
+
 (defun main (matrix)
   (let ((resultList '())
         (indexTable (gen-index-table matrix))
@@ -97,5 +113,6 @@
         (colNum (length (car matrix))))
     (loop for r from 0 to (1- rowNum) do
          (loop for c from 0 to (1- colNum) do
-              (
-|#
+                (setf resultList (append resultList (list (sum-list
+                                                           (tree-search-matrix indexTable r c)))))))
+    (return-from main (max-list resultList))))
