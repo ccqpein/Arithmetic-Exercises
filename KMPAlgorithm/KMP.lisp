0f2d0b6cf76914bae58fdc07aae868138fcf4273
ccQpein <ccqpein@protonmail.com>
2016-03-16 16:13:28 -0400

    finish find index number

--- KMPAlgorithm/KMP.lisp
+++ KMPAlgorithm/KMP.lisp
@@ -20,11 +20,21 @@
          (setf result (append result (list (subseq strList 0 i)))))
     result))
 
+(defun max-list (list)
+      (loop for element in list maximizing element))
+
 (defun get-index-number (str)
   (let ((pre-table (get-pattern-from-head str))
         (bot-table (reverse (get-pattern-from-back str)))
         (indexNum 0))
     ;(print pre-table)
     ;(print bot-table)
-    (mapcar #'equal pre-table bot-table)
-    ())
+    (setf indexNum
+          (mapcar #'(lambda (list1 list2)
+                      (let ((num 0))
+                        (loop for i from 0 to (1- (length list1)) do
+                             (if (equal (nth i list1) (nth i list2))
+                                 (setf num (length list1))))
+                        num)) pre-table bot-table))
+    ;(print indexNum)
+    (setf indexNum (max-list indexNum))))
