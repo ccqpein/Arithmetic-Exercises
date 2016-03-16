6c18e36b54d4caa81a415b770a69be4dca358988
ccQpein <ccqpein@protonmail.com>
2016-03-16 15:13:57 -0400

    add function to get index table

--- KMPAlgorithm/KMP.lisp
+++ KMPAlgorithm/KMP.lisp
@@ -1,4 +1,21 @@
 (defpackage #:KMP
-  (:ues #:CL))
+  (:use #:CL))
 
 (in-package #:KMP)
+
+;;; make the string index table
+(defun get-pattern-from-back (str)
+  (let ((result '())
+        (strList (coerce str 'list)))
+    (loop for i on strList do
+         ;(print i)
+         (setf result (append result (list i))))
+    (cdr result)))
+
+(defun get-pattern-from-head (str)
+  (let ((result '())
+        (strList (coerce str 'list)))
+    (loop for i from 1 to (1- (length strList)) do
+         ;(print strList)
+         (setf result (append result (list (subseq strList 0 i)))))
+    result))
---
+++ KMPAlgorithm/README.md
@@ -0,0 +1,3 @@
+# README
+
+Write KMP algorithm follow this [blog](http://blog.csdn.net/v_july_v/article/details/7041827)
