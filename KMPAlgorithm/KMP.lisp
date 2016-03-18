a9fead1fb4f5cc5a7d8486cfa10089f356e1da42
ccQpein <ccqpein@protonmail.com>
2016-03-18 14:16:09 -0400

    add

--- KMPAlgorithm/KMP.lisp
+++ KMPAlgorithm/KMP.lisp
@@ -27,17 +27,29 @@
   (let ((pre-table (get-pattern-from-head str))
         (bot-table (reverse (get-pattern-from-back str)))
         (indexNum 0))
-    ;(print pre-table)
-    ;(print bot-table)
+    (print pre-table)
+    (print bot-table)
     (setf indexNum
           (mapcar #'(lambda (list1 list2)
                       (let ((num 0))
-                        (loop for i from 0 to (1- (length list1)) do
-                             (if (equal (nth i list1) (nth i list2))
-                                 (setf num (length list1))))
-                        num)) pre-table bot-table))
-    ;(print indexNum)
+                        (if (equal list1 list2)
+                            (setf num (length list1)))
+                  num)) pre-table bot-table))
+    (print indexNum)
     (setf indexNum (max-list indexNum))))
 
+(defun make-index-table (p)
+  (let ((pList (coerce p 'list))) 
+    (do ((subStr (car pList) (list subStr (car restStr)))
+         (restStr (cdr pList) (cdr restStr)))
+        ((
+
 (defun do-kmp (s p)
-  )
+  (let* ((strList (coerce s 'list))
+         (patList (coerce p 'list))
+         (indexTable (get-index-number p)))
+    (print strList)
+    (print patList)
+    (print indexTable)#|
+    (do ((sPoint (car strList))
+         (pPoint (car |#))
