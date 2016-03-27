6d1392df4de8fc109236df99f0b2ecc9a9ff3645
ccQpein <ccqpein@protonmail.com>
2016-03-27 15:41:56 -0400

    keep working

---
+++ binary-tree/README.md
@@ -0,0 +1,6 @@
+# README #
+
+I wanna add some algrithm here:
+
+  * Basic Binary Tree
+  * AVL Tree
--- binary-tree/basicBT.py
+++ binary-tree/basicBT.py
@@ -11,7 +11,7 @@ class Node():
                 self.left = Node(data)
             else:
                 self.left.insert(data)
-        if data > self.data:
+        elif data > self.data:
             if self.right is None:
                 self.right = Node(data)
             else:
@@ -27,11 +27,35 @@ class Node():
                 return None, None
             return self.right.lookup(data, self)
         else:
-            return self.data, parent
+            return self, parent
+
+    def children_count(self):
+        cnt = 0
+        if self.left:
+            cnt += 1
+        if self.right:
+            cnt += 1
+        return cnt
+
+    def delete(self, data):
+        node, parent = self.lookup(data)
+
+        if node is not None:
+            children_count = node.children_count()
+            if children_count == 0:
+                if parent.left is node:
+                    parent.left = None
+                else:
+                    parent.right = None
+                del node
 
 
-def main():
-    r = Node(8)
-    r.insert(3)
-    r.insert(10)
-    r.insert(1)
+root = Node(8)
+root.insert(3)
+root.insert(10)
+root.insert(1)
+root.insert(6)
+root.insert(4)
+root.insert(7)
+root.insert(14)
+root.insert(13)
