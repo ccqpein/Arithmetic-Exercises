506a9597eb2a803b11a237ce5737465da078838d
ccQpein <ccqpein@protonmail.com>
2016-05-12 14:27:55 -0400

    still learning

--- Add-Digits/add-digits.swift
+++ Add-Digits/add-digits.swift
@@ -1,13 +1,29 @@
-func AddDigit(numStr:Int) -> Int {
-    var reNum:Int
-    let temp:String = String(numStr)
-    for char in temp.characters {
-        reNum += Int(char)
+func AddDigit(numIn:Int) -> Int {
+    //var reNum:Int?
+    let temp:String = String(numIn)
+    var charList = [String]()
+    for c in temp.characters {
+        let cc = String(c)
+        charList += [cc]
+        print(charList)
+    }
+
+    return 10
+}
+
+func main(){
+
+    AddDigit(15)
+}
+    /*
+    for c in charList {
+        reNum = Int(c) + reNum
     }
 
     if reNum<10 {
         return reNum
     }else{
         reNum = AddDigit(reNum)
-    }
-}
+        return reNum
+    }*/
+
