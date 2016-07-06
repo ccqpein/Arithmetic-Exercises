d4ee5f2c73b4856f4ccf171c93ea12167ef0be8a
ccQpein <ccqpein@protonmail.com>
2016-07-06 14:31:14 -0400

    finish

---
+++ Set-matrix-zero/SMZ.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func setZeroes(matrix [][]int) {
+	lengthM := len(matrix)
+	colLen := len(matrix[0])
+
+	var zeroRow = make([]int, colLen)
+	var colZeroIndex []int
+
+	for i := 0; i < lengthM; i++ {
+		testI := 0
+		for colIndex, ele := range matrix[i] {
+			if ele == 0 {
+				colZeroIndex = append(colZeroIndex, colIndex)
+				testI += 1
+			}
+		}
+		if testI != 0 {
+			matrix[i] = zeroRow
+		} else {
+			for _, ii := range colZeroIndex {
+				matrix[i][ii] = 0
+			}
+		}
+
+	}
+	fmt.Print(matrix)
+}
+
+func main() {
+	matrix := [][]int{
+		[]int{1, 2, 0, 3, 4, 5},
+		[]int{0, 4, 5, 6, 7, 8},
+		[]int{1, 2, 3, 4, 5, 6},
+	}
+	setZeroes(matrix)
+}
