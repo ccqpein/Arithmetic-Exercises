5c3ec924339906591665ffd3c5c14235c4e82528
ccQpein <ccqpein@protonmail.com>
2016-07-11 13:34:03 -0400

    done small function

--- KMPAlgorithm/KMP.go
+++ KMPAlgorithm/KMP.go
@@ -4,5 +4,28 @@ import (
 	"fmt"
 )
 
+var testS string = "BBC ABCDAB ABCDABCDABDE"
+var testP string = "ABCDABD"
 
-func
+func makeIndexTable(pattern string) []int {
+	var indexTable []int
+	for i := 1; i <= len(pattern); i++ {
+		thisP := pattern[:i] //not sure
+		tempLen := 0
+		for ii := 1; ii < i; ii++ {
+			if thisP[:ii] == thisP[i-ii:] &&
+				len(thisP[:ii]) > tempLen {
+				tempLen = len(thisP[:ii])
+			}
+		}
+		indexTable = append(indexTable, tempLen)
+	}
+	return indexTable
+}
+
+func main() {
+	fmt.Print(
+		makeIndexTable("abcda"),
+		makeIndexTable("abcdab"))
+
+}
