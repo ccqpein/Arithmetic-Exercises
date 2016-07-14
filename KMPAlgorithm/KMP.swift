cf01870a13747649f0324413ca718893f62cd5f5
ccQpein <ccqpein@protonmail.com>
2016-07-14 18:58:26 -0400

    meet some issues on string iter

--- KMPAlgorithm/KMP.swift
+++ KMPAlgorithm/KMP.swift
@@ -1,4 +1,19 @@
+import Foundation
+
 var testS:String = "bbc abcdab abcdabcdabde"
 var testP:String = "abcdabd"
 
 print(testS, testP)
+
+/*func makeIndexTable(pattern:String) {
+
+    for i in  {
+        print(pattern[i])
+             }
+                                  
+}*/
+
+//print(Array(testP.characters))
+print(testP.endIndex)
+let myRange = testP.startIndex..<testP.index(testP.startIndex, offsetBy:4)
+print(testP.substring(with:myRange))
