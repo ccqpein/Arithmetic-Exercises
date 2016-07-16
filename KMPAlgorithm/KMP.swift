f1c266dbbab847d0b91e48f82610882b3695c5b6
ccQpein <ccqpein@protonmail.com>
2016-07-16 00:34:15 -0400

    finish index table

--- KMPAlgorithm/KMP.swift
+++ KMPAlgorithm/KMP.swift
@@ -5,17 +5,30 @@ import Foundation
 var testS:String = "bbc abcdab abcdabcdabde"
 var testP:String = "abcdabd"
 
-print(testS, testP)
+func makeIndexTable(pattern: String) -> Array<Int>{
 
-/*func makeIndexTable(pattern:String) {
-
-    for i in  {
-        print(pattern[i])
+    let patternA = Array(pattern.characters)
+    var indexTable:Array<Int> = [-1,]
+    for i in 1..<patternA.count {
+                     let thisP = patternA[0...i]
+                     //print(thisP)
+                     var tempLen = 0
+                     for ii in 0..<i {
+                                       //print(thisP[0...ii], thisP[i-ii...i])
+                                       if (thisP[0...ii] == thisP[i-ii...i])
+                                          && (thisP[0...ii].count > tempLen) {
+                                           tempLen = thisP[0...ii].count
+                                       }
+                               }
+                                   indexTable.append(tempLen)
              }
+                 return indexTable
                                   
-}*/
+}
+
+print(makeIndexTable(pattern:testP))
 
 //print(Array(testP.characters))
-print(testP.endIndex)
-let myRange = testP.startIndex..<testP.index(testP.startIndex, offsetBy:4)
-print(testP.substring(with:myRange))
+//print(testP.endIndex)
+//let myRange = testP.startIndex..<testP.index(testP.startIndex, offsetBy:4)
+//print(testP.substring(with:myRange))
