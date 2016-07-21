73e041f9e2087f9f945ca87dcce598f6de777232
ccQpein <ccqpein@protonmail.com>
2016-07-20 20:22:04 -0400

    add test example

---
+++ Dijkstra-algorithm/DA.swift
@@ -0,0 +1,20 @@
+let example1:[[Int?]] = [
+    [0, 6, 3, nil, nil, nil,],
+    [6, 0, 2, 5, nil, nil,],
+    [3, 2, 0, 3, 4, nil,],
+    [nil, 5, 3, 0, 2, 3,],
+    [nil, nil, 4, 2, 0, 5],
+    [nil, nil, nil, 3, 5, 0,],
+]
+
+let example2:[[Int?]] = [
+    [0, 7, 9, nil, nil, 14,],
+    [7, 0, 10, 15, nil, nil,],
+    [9, 10, 0, 11, nil, 2],
+    [nil, 15, 11, 0, 6, nil,],
+    [nil, nil, nil, 6, 0, 9,],
+    [14, nil, 2, nil, 9, 0,],
+]
+
+print(example1)
+print(example2)
