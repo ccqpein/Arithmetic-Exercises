b22ced61bb8265e54e7e7de6255583f03c205756
ccQpein <ccqpein@protonmail.com>
2016-08-01 16:23:32 -0400

    finish new

--- Number-of-1-Bits/No1B.swift
+++ Number-of-1-Bits/No1B.swift
@@ -1,3 +1,10 @@
 func ham(_ num:Int) -> Int {
-    var oneSet = 0
+    var oneSet = 1
+    var n = num
+    while(n>1) {
+        oneSet += n%2
+        n = n/2
+        print(n)
+    }
+    return oneSet
 }
