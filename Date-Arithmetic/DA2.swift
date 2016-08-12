e52c0ebbf2208d7bf219e6a62019e0222b590d53
ccQpein <ccqpein@protonmail.com>
2016-08-12 10:22:59 -0500

    start new

---
+++ Date-Arithmetic/DA2.swift
@@ -0,0 +1,19 @@
+// Use different design
+
+let monthDay:[Int] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,]
+let monthDayL:[Int] = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,]
+let yzero = 2000
+
+class Date {
+    var d:Int
+    var m:Int
+    var y:Int
+    
+    func leapYear(_ y:Int) -> Bool {
+        if (y % 4 == 0) && (y % 400 == 0 || y % 100 != 0) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
