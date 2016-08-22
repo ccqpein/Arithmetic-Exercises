e08b954890857cbd41581d94a27f73aafa6eed9e
ccQpein <ccqpein@protonmail.com>
2016-08-22 16:47:02 -0400

    a bit

--- Mini-Parser/MP.go
+++ Mini-Parser/MP.go
@@ -22,13 +22,18 @@ func (n *NestedInteger) Add(elem NestedInteger) {}
 func (n NestedInteger) GetList() []*NestedInteger {}
 
 func deserialize(s string) *NestedInteger {
-	if s[:-1] != "]" {
+	if s[0] != "[" {
 		val, _ := strconv.Atoi(s)
 		a := NestedInteger{value: val}
 		return &a
 	} else {
+		ss := s[1 : len(s)-1]
+		inTemp := string("")
+		for i, v := range ss {
 
+		}
 	}
+
 }
 
 func main(s string) {
