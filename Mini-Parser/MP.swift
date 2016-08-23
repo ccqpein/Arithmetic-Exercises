755e2fc9720632c2da6907c152be46c07ba70588
ccQpein <ccqpein@protonmail.com>
2016-08-23 10:51:22 -0400

    start swift version

---
+++ Mini-Parser/MP.swift
@@ -0,0 +1,11 @@
+struct NestedInteger {
+    var value:Int
+    var nestedL:NestedInteger
+}
+
+func deserialize(_ s:String) -> NestedInteger {
+    var str = Array(s)
+    if str[0] != "[" {
+        var val = 
+    }
+}
