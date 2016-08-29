6c3dbef0a69f681f548c86c06a83e142ec91b58c
ccQpein <ccqpein@protonmail.com>
2016-08-29 15:44:14 -0400

    finish lisp version

--- Mini-Parser/MP.lisp
+++ Mini-Parser/MP.lisp
@@ -1,3 +1,37 @@
 (defvar test1 "324")
 (defvar test2 "[123,[456,[789]]]")
 
+(defun unpack (s)
+  "unpack the string, return list"
+  (let ((sl (coerce s 'list)))
+    (if (eql (first sl) #\[)
+        (cdr (butlast sl))
+        sl)
+    ))
+
+(defun get-number-string (sl)
+  "get the first numbers string from unpacking parser"
+  (coerce
+   (loop for i in sl
+      while (digit-char-p i)
+      collect i) 'string))
+
+(defun get-rest-string (sl)
+  (do ((ii 0 (incf ii)))
+      ((or (eql (nth ii sl) #\[) (= ii (length sl)))
+       (coerce (subseq sl ii) 'string))))
+
+(defstruct NestedInteger
+  value
+  (Nested nil)
+  )
+
+(defun deserialize (s)
+  (let ((sl (unpack s))
+        (aa (make-NestedInteger)))
+    (setf (NestedInteger-value aa) (get-number-string sl))
+    (if (eql (car (last sl)) #\])
+        (setf (NestedInteger-Nested aa)
+              (deserialize (get-rest-string sl))))
+    aa
+    ))
