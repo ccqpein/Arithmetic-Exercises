cc86bffaf1d427a8d068c835411fcc7e8968fcec
ccQpein <ccqpein@protonmail.com>
2016-08-31 09:56:30 -0400

    something wrong

--- Find-the-Difference/FtD.py
+++ Find-the-Difference/FtD.py
@@ -1,13 +1,26 @@
-l = [i + 1 for i in range(9)]
+def findTheDifference(s, t):
+    """
+    :type s: str
+    :type t: str
+    :rtype: str
+    """
+    sDic = {}
+    tDic = {}
+    for i in range(len(s)):
+        try:
+            sDic[s[i]] += 1
+        except KeyError:
+            sDic[s[i]] = 1
 
-while True:
-    lleng = len(l)
-    tempL = []
-    for i in range(lleng):
-        if i % 2 == 0:
-            tempL.append(l[i])
+    for i in range(len(t)):
+        try:
+            tDic[t[i]] += 1
+        except KeyError:
+            tDic[t[i]] = 1
 
-    l = tempL.reverse()
-    print(l)
-    if len(l.sort()) == 1:
-        break
+    for i in tDic.items():
+        try:
+            if i[1] != sDic[i[0]]:
+                return i[0]
+        except KeyError:
+            return i[0]
