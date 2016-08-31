24cb9c6a919b0e596bdbf5faa9d560b0dc349224
ccQpein <ccqpein@protonmail.com>
2016-08-30 20:18:21 -0400

    finish lisp version

---
+++ Elimination-Game/EG.lisp
@@ -0,0 +1,13 @@
+(defun filter-sequence (l)
+  (loop for i from 1 to (- (length l) 1) by 2
+     collect (nth i l)))
+
+(defun last-remaining (n)
+  (let ((l (loop for i from 1 to n collect i)))
+    (labels ((temp (ll)
+               ;(print ll)
+               (if (/= (length ll) 1)
+                   (temp (reverse (filter-sequence ll)))
+                   (car ll))))
+      (temp l))))
+
