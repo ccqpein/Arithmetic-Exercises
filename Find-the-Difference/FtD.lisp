50c8e306279dc86fa0cd2a47879569fdb7423fe9
ccQpein <ccqpein@protonmail.com>
2016-08-31 11:19:19 -0400

    finish lisp version

---
+++ Find-the-Difference/FtD.lisp
@@ -0,0 +1,21 @@
+(defmacro hash+1 (key hashT)
+  "plus 1 if key-vaule existed, else new let key-value be 1"
+  `(if (gethash ,key ,hashT)
+      (incf (gethash ,key ,hashT))
+      (setf (gethash ,key ,hashT) 1)))
+
+(defun make-ftd-hash (s)
+  (let ((sh (make-hash-table)))
+    (loop for c across s
+       do (hash+1 c sh))
+    sh))
+
+(defun find-the-difference (s p)
+  (let ((sT (make-ftd-hash s))
+        (pT (make-ftd-hash p)))
+    (loop for k being the hash-key of pT
+       when (/= (gethash k pT)
+                (if (gethash k sT)
+                    (gethash k sT)
+                    0))
+       do (print k))))
