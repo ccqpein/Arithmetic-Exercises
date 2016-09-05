f769e3ffb6fb0aa25dc7db677fb4e6d34e32e978
ccQpein <ccqpein@protonmail.com>
2016-09-05 15:29:13 -0400

    finish swift version

--- Find-the-Difference/Ftd.swift
+++ Find-the-Difference/Ftd.swift
@@ -1,1 +1,39 @@
-func
+let s1 = "abc"
+let p1 = "abcd"
+
+let s2 = "aaa"
+let p2 = "abaa"
+
+func FindTheDifferent(_ s:String, _ p:String) -> Character {
+    var sT:[Character:Int] = [:]
+    var pT:[Character:Int] = [:]
+
+    for c in s.characters {
+        if sT[c] == nil {
+            sT[c] = 1
+        }else{
+            sT[c] = sT[c]! + 1
+        }
+    }
+
+    for c in p.characters {
+        if pT[c] == nil {
+            pT[c] = 1
+        }else{
+            pT[c] = pT[c]! + 1
+        }
+    }
+
+    for (c,v) in pT {
+        if sT[c] == nil || sT[c] != v {
+            return c
+        }
+    }
+
+    return Character("")
+}
+
+
+print(FindTheDifferent(s1,p1))
+print(FindTheDifferent(s2,p2))
+
