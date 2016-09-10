c7b5c1993fb55b6699ce6a6fabd20af2c6e34dfe
ccQpein <ccqpein@protonmail.com>
2016-09-09 22:07:46 -0400

    a bit

---
+++ Sqrt-x/README.md
@@ -0,0 +1,6 @@
+# README
+
+Implement int sqrt(int x).
+
+Compute and return the square root of x.
+
---
+++ Sqrt-x/S.py
@@ -0,0 +1,7 @@
+def mySqrt(x):
+    if x == 0:
+        return 0
+    else x == 1:
+        return 1
+
+    # use 1
