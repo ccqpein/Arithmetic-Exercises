4a9ddd6e97f14dc5497f1396ecc33bc578e93bdd
ccQpein <ccqpein@protonmail.com>
2016-09-10 13:24:42 -0400

    finish python version && start new

--- Sqrt-x/S.py
+++ Sqrt-x/S.py
@@ -1,7 +1,14 @@
 def mySqrt(x):
     if x == 0:
         return 0
-    else x == 1:
+    elif x == 1:
         return 1
 
     # use 1
+    result = 0
+    result2 = 1  # = 1.0 if using in python2
+    while result2 != result:
+        result = result2
+        result2 = (result + x / result) / 2
+
+    return int(result2)
