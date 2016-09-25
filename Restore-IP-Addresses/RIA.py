6d8ec81615dd6fb4feb76dafafd6e79e80ed37e3
ccQpein <ccqpein@protonmail.com>
2016-09-25 12:00:00 -0400

    a bit (auto)

--- Restore-IP-Addresses/RIA.py
+++ Restore-IP-Addresses/RIA.py
@@ -3,15 +3,20 @@ testStr = "25525511135"
 
 class Solution(object):
 
-    def test(self, s):
-        print(s)
+    def getNext(self, sPre, sLeft):
+        preSL = [sPre, ]
+        tempStore = []
+        for i in range(3):
+            if int(sLeft[i + 1]) < 3:
+                tempStore.append(sLeft[0:i + 1])
+        return [preSL + [ss] for ss in tempStore]
 
     def restoreIpAddresses(self, s):
         """
         :type s: str
         :rtype: List[str]
         """
-        self.test(s)
+        print(self.getNext("", s))
 
 if __name__ == "__main__":
     a = Solution()
