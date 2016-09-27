5197887a8411b5bfa023a3281d045f90e63bfc9c
ccQpein <ccqpein@protonmail.com>
2016-09-27 12:00:00 -0400

    a bit (auto)

--- Restore-IP-Addresses/RIA.py
+++ Restore-IP-Addresses/RIA.py
@@ -1,4 +1,9 @@
 testStr = "25525511135"
+testStr2 = "0000"
+testStr3 = "010010"
+# answer 3: ["0.10.0.10","0.100.1.0"]
+testStr4 = "101023"
+# answer 4: ["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]
 
 
 class Solution(object):
@@ -6,8 +11,11 @@ class Solution(object):
     def getNext(self, sLeft):
         tempStore = []
         for i in range(3):
-            if int(sLeft[i + 1]) < 3:
+            try:
+                # if int(sLeft[i + 1]) < 3:
                 tempStore.append(sLeft[0:i + 1])
+            except:
+                continue
         return [[ss] + [sLeft[len(ss):]] for ss in tempStore]
 
     def parseList(self, l):
@@ -29,13 +37,20 @@ class Solution(object):
                     tempList.append(
                         l[:-1] + ll)
             listPool = tempList
-#        print(listPool)
 
         reList = []
         for l in listPool:
+            if l[-1] == "":
+                continue
+            thisBool = True
             for ii in l:
-                if int(ii) > 255:
-            reList.append(self.parseList(l))
+                if int(ii) > 255 or \
+                   len(ii) > 3 or \
+                   (len(ii) > 1 and ii[0] == "0"):
+                    thisBool = False
+            if thisBool is True:
+                # print(l)
+                reList.append(self.parseList(l))
 
         return reList
 
