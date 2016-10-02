9fa9154b6adda21ec1cb35d38d365a62cad39a6f
ccQpein <ccqpein@protonmail.com>
2016-10-02 18:19:08 -0400

    a bit

--- Restore-IP-Addresses/RIA.swift
+++ Restore-IP-Addresses/RIA.swift
@@ -6,13 +6,13 @@ let testStr4 = "101023"
 class Solution {
     func cutIp(_ s:String) -> [[String]] {
         let lenStr:Int = s.characters.count
-        var iterTime:Int = min(lenStr, 4)
+        let iterTime:Int = min(lenStr, 4)
         var reStrList = [[String]]()
 
         for i in 1...(iterTime - 1) {
-            var startIndex = s.startIndex
-            var endIndex = s.endIndex
-            var cutIndex = s.index(s.startIndex, offsetBy:i)
+            let startIndex = s.startIndex
+            let endIndex = s.endIndex
+            let cutIndex = s.index(s.startIndex, offsetBy:i)
 
             reStrList.append(
               [s[startIndex..<cutIndex],s[cutIndex..<endIndex]]
@@ -23,11 +23,26 @@ class Solution {
     }
 
     func nextCut(_ strL:[String]) -> [[String]] {
-        
+        if strL.count == 1{
+            return cutIp(strL.last!)
+        }
+
+        if strL.last!.count == 1{
+            return Array<[String]>(strL)
+        }
+
+        var reStrList = [[String]]()
+        for i in cutIp(strL.last!) {
+            reStrList.append(
+              Array(strL.dropLast(),i))
+        }
     }
     
-    func restoreIpAddresses(_ s: String) -> [String] {
+    func restoreIpAddresses(_ s:String) -> [String] {
         return ["a"]
     }
 }
 
+
+var a = Solution()
+a.nextCut(["0", "000"])
