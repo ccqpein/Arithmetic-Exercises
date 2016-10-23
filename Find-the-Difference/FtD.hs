c71dbab5beaab4f0b1ae6f3ed3461111f4f4f595
ccQpein <ccqpein@protonmail.com>
2016-10-23 14:01:56 -0400

    finish haskell version

--- Find-the-Difference/FtD.hs
+++ Find-the-Difference/FtD.hs
@@ -1,8 +1,18 @@
 import qualified Data.Map.Lazy as M
 
-mm = M.empty
-
 hashAdd1 :: (Ord k, Num a) => k -> M.Map k a -> M.Map k a
 hashAdd1 k m
   | M.member k m = M.insertWith (+) k 1 m
   | otherwise = M.insert k 1 m
+
+makeFTDhash :: (Ord k, Num a) => [k] -> M.Map k a -> M.Map k a
+makeFTDhash [] m     = m
+makeFTDhash (k:ks) m = makeFTDhash ks (hashAdd1 k m)
+
+diff :: (Ord k, Eq a) => [k] -> M.Map k a -> M.Map k a -> k
+diff (k:ks) m mm
+  |(lookup k $ M.toList m) /= (lookup k $ M.toList mm) = k
+  |otherwise = diff ks m mm
+
+findTheDiff :: (Ord k) => [k] -> [k] -> k
+findTheDiff s t  = diff (M.keys $ makeFTDhash t M.empty) (makeFTDhash s M.empty) (makeFTDhash t M.empty)
