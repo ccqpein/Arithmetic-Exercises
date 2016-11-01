ee4e8adec05b4b50811b3997a0f3e9956d6d3c42
ccQpein <ccqpein@protonmail.com>
2016-11-01 14:28:11 -0400

    still has problems

--- Climbing-Stairs/CS.hs
+++ Climbing-Stairs/CS.hs
@@ -2,9 +2,5 @@ permutations' :: (Fractional a, Enum a, Num a) => a -> a -> a
 permutations' a b = let aa = a-b+1 in
   (product [aa..a])/ (product [1..b])
 
-genList :: (Fractional a, Enum a, Num a, Integral a) => a -> [a]
-genList a = [1..(div a 2)]
-
 climbS :: (Fractional a, Enum a, Num a, Integral a) => a -> a
-climbS a = let llist = (genList a) in
-  sum [permutations' a n | n <- llist]
+climbS nn = sum [(permutations' n (n + y))| n <- [1..(div nn 2)], let y = (nn - n * 2)]
