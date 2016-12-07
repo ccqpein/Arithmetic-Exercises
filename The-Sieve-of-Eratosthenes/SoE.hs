a6b9861a09c1f53433e9745f77f9523b0300eea9
ccQpein <ccqpein@protonmail.com>
2016-12-07 13:55:27 -0500

    a bit

--- The-Sieve-of-Eratosthenes/SoE.hs
+++ The-Sieve-of-Eratosthenes/SoE.hs
@@ -1,3 +1,11 @@
-soe :: Int -> [Int]
-soe n = endNum = (ceiling.sqrt.fromIntegral) n
+filterCond :: Int -> Int -> Bool
+filterCond n n2 = if ((==0).(`mod`n)) n2
+                  then (/=n) n2
+                  else False
+
+filterL :: Int -> [Int] -> [Int]
+filterL n nL = filter ((not).(filterCond n)) nL
+
+
+--endNum = (ceiling.sqrt.fromIntegral) n
 
