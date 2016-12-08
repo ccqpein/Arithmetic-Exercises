c74ea547f5e4296ecfd72af7de73d6378d6976c4
ccQpein <ccqpein@protonmail.com>
2016-12-08 12:34:49 -0500

    finish haskell version

--- The-Sieve-of-Eratosthenes/SoE.hs
+++ The-Sieve-of-Eratosthenes/SoE.hs
@@ -6,6 +6,11 @@ filterCond n n2 = if ((==0).(`mod`n)) n2
 filterL :: Int -> [Int] -> [Int]
 filterL n nL = filter ((not).(filterCond n)) nL
 
+endNum :: Int -> [Int]
+endNum n = [2..((ceiling.sqrt.fromIntegral) n)]
 
---endNum = (ceiling.sqrt.fromIntegral) n
+soe :: [Int] -> [Int] -> [Int]
+soe (x:xs) nL = let nLL = filterL x nL in
+                  soe xs nLL
+soe [] nL = tail nL
 
