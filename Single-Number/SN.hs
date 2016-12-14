c0fdbed8c6964e9d2732acba3aa4c3a9a01e48d4
ccQpein <ccqpein@protonmail.com>
2016-12-14 12:06:02 -0500

    a bit

--- Single-Number/SN.hs
+++ Single-Number/SN.hs
@@ -1,9 +1,13 @@
 import qualified Data.Map.Lazy as M
 
-let hs = M.empty
 
 makeTable :: Int -> M.Map Int Int -> M.Map Int Int
 makeTable a hs
-  | M.member a hs = M.update a ((1+) $ lookup a hs)
+  | M.member a hs = M.update (\x -> Just (x+1)) a hs
   | otherwise = M.insert a 1 hs
+
+makeL2T :: [Int] -> M.Map Int Int -> M.Map Int Int
+makeL2T [] hs     = hs
+makeL2T (x:xs) hs = makeTable x (makeL2T xs hs)
+
 
