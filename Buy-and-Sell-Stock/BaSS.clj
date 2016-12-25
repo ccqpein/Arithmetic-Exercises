8fb65f14506e36307d1166516fff0b26baa9c65b
ccQpein <ccqpein@protonmail.com>
2016-12-25 11:23:10 -0500

    finish clojure version

--- Buy-and-Sell-Stock/BaSS.clj
+++ Buy-and-Sell-Stock/BaSS.clj
@@ -5,18 +5,20 @@
 (def *test5 '(2 4 1))
 
 (defn max-profit [prices]
-  (def ^:dynamic maxv 0)
-  (def ^:dynamic minv (first prices))
-  (def ^:dynamic result 0)
-  (def restl (pop prices))
-  (doseq [i restl]
-    (println i)
-    (cond
-      (< i minv) (set! minv i)
-      (> (- i minv) result) (do (set! maxv i)
-                                (set! result (- maxv minv))
-                                (print "hi" result)))
-    (print "{" maxv minv "}"))
-  (set! result (if (< result 0) 0 result))
-  result)
+  (def ^:dynamic maxv)
+  (def ^:dynamic minv)
+  (def ^:dynamic result)
+  (def restl (try (pop prices)
+                  (catch Exception e "0")))
+  (try
+    (binding [maxv 0
+              minv (first prices)
+              result 0]
+      (doseq [i restl]
+        (cond
+          (< i minv) (set! minv i)
+          (> (- i minv) result) (do (set! maxv i)
+                                    (set! result (- maxv minv)))))
+      result)
+    (catch Exception e "0")))
 
