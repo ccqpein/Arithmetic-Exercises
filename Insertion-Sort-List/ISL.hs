3b171a08d94a46b8aa580306debab5fe27dce79c
ccQpein <ccqpein@protonmail.com>
2016-12-26 00:02:02 -0500

    finish haskell version

--- Insertion-Sort-List/ISL.hs
+++ Insertion-Sort-List/ISL.hs
@@ -1,6 +1,17 @@
 data ListNode a = Empty | ListNode a (ListNode a) deriving (Show, Read)
+-- actually, make a new data type is not necessary, so I comment ths code below
 
+{--
 b = ListNode 1 Empty
 a = ListNode 2 b
 c = ListNode 3 a
+--}
+
+insertionSList :: [Int] -> [Int] -> [Int]
+insertionSList a [] = a
+insertionSList a (x:xs) = insertionSList (insertList a x) xs where
+  insertList [] a = a:[]
+  insertList (x:xs) a
+    | a < x = a:x:xs
+    | a >= x = x:(insertList xs a)
 
