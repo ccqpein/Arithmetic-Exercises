a3efdff44e6636e5193d98839cadf02327b39a70
ccQpein <ccqpein@protonmail.com>
2017-01-02 13:09:33 -0500

    finish swift version, wait, there is a bug

--- Perfect-Squares/PS.swift
+++ Perfect-Squares/PS.swift
@@ -9,12 +9,22 @@ func geneList(_ numInput:Int) -> [Int] {
 func ps(_ numInput:Int) -> Int{
     guard numInput != 0 else {return 0}
 
-    let squareL = geneList(numInput)
-    var timeRe = 0
-    for i in squareL.reverse() {
-        if i <= numInput {
-            timeRe += 1
-            
+    var squareL = geneList(numInput)
+
+    func innerPs(_ n:Int, _ sL:[Int]) -> Int{
+        guard n != 0 else {return 0}
+        var result = 1
+        for i in sL.reversed() {
+            if i <= n {
+                print(i)
+                result += innerPs((n-i),sL)
+                break
+            }
         }
+        return result
     }
+    var result = innerPs(numInput,squareL)
+    return result
 }
+
+//print(ps(12)) //result should be 3, but I wrote wrong
