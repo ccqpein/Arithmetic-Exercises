ff36053fe7a778b318f8e087b225e790a539b198
ccQpein <ccqpein@protonmail.com>
2017-01-07 14:06:53 -0500

    change design

--- Total-Hamming-Distance/THD.hs
+++ Total-Hamming-Distance/THD.hs
@@ -1,7 +1,7 @@
 diffHam :: (Integral a) => a -> a -> a
 diffHam a b
   | a == 0 && b == 0 = 0
-  | otherwise = if (mod a 2) /= (mod b 2)
-                then 1 + diffHam (div a 2) (div b 2)
-                else 0 + diffHam (div a 2) (div b 2)
-
+  | otherwise = let rr = if (mod a 2) /= (mod b 2)
+                      then 1
+                      else 0
+                in rr + diffHam (div a 2) (div b 2)
