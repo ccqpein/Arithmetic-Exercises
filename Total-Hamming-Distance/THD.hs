9abf32285ac7853bed54d70ae3e63f9411041a0e
ccQpein <ccqpein@protonmail.com>
2017-01-08 12:12:59 -0500

    finish haskell version

--- Total-Hamming-Distance/THD.hs
+++ Total-Hamming-Distance/THD.hs
@@ -5,3 +5,7 @@ diffHam a b
                       then 1
                       else 0
                 in rr + diffHam (div a 2) (div b 2)
+
+totalHammingDistance :: (Integral a) => [a] -> a
+totalHammingDistance []     = 0
+totalHammingDistance (x:xs) = (sum $ map (diffHam x) xs) + totalHammingDistance xs
