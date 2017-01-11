01072f8af0b3e763757128f50d286a548203a9b0
ccQpein <ccqpein@protonmail.com>
2017-01-11 11:42:51 -0500

    a bit

--- Total-Hamming-Distance/THD.clj
+++ Total-Hamming-Distance/THD.clj
@@ -1,31 +1,3 @@
-(defn diff-ham  [x y]
-  (let [re 0
-        xin x
-        yin y]
-    (while (or (pos? @xin) (pos? @yin))
-      (do 
-        (cond (not= (mod xin 2) (mod yin 2))
-              (swap! re inc))
-        (update-in xin - (mod xin 2))
-        (update-in xin / 2)
-        (update-in yin - (mod yin 2))
-        (update-in yin / 2)))
-    re))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+(diff-ham [x y]
+          )
 
