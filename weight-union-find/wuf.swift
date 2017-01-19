c5864ce82122171d6c2983fe6f3d9f7ab99b0eb1
ccQpein <ccqpein@protonmail.com>
2017-01-19 16:15:30 -0500

    a bit

--- weight-union-find/wuf.swift
+++ weight-union-find/wuf.swift
@@ -1,13 +1,16 @@
 class Node{
     var value:Int
-    var node:Node?
+    var node:[Node]?
     var root:Node?
+    var numberOfNode = 0
 
     init(_ initvalue:Int){
         self.value = initvalue
     }
 }
 
-func connected(_ a:Node, _ b:Node)  -> [Int] {
-    
+func connected(_ a:Node, _ b:Node){
+    a.node.append(b)
+    a.numberOfNode += 1
+    b.root = a
 }
