ed829dadfb56f4dc59f2f52cabff1a9b7f1f11ed
ccQpein <ccqpein@protonmail.com>
2017-01-20 20:59:21 -0500

    a bit

--- weight-union-find/wuf.swift
+++ weight-union-find/wuf.swift
@@ -2,7 +2,7 @@ class Node{
     var value:Int
     var node:[Node]?
     var root:Node?
-    var numberOfNode = 0
+    var numberOfLevel = 0
 
     init(_ initvalue:Int){
         self.value = initvalue
@@ -10,6 +10,18 @@ class Node{
 }
 
 func connected(_ a:Node, _ b:Node){
+    switch (a.root, b.root){
+    case let (nil,nil):
+        if a.numberOfLevel >= b.numberOfLevel{
+            a.node.append(b)
+        }else{
+            b.node.append(a)
+        }
+    case let (x,nil):
+        x.node.append(b)
+    }
+      
+    
     a.node.append(b)
     a.numberOfNode += 1
     b.root = a
