d626f85fef6e309714282803cb91778033f3ce99
ccQpein <ccqpein@protonmail.com>
2017-01-24 10:57:11 -0500

    use haskell tree module

--- weight-union-find/wuf.hs
+++ weight-union-find/wuf.hs
@@ -1,15 +1,17 @@
-data Node = Empty
-          | Node { value         :: Int
-                 , nodeL         :: [Node]
-                 , root          :: Node
-                 , numberOfLevel :: Int
-                 } deriving (Show, Eq)
+import           Data.Tree
 
-findRoot :: Node -> Node
-findRoot a
-  | root a == Empty = a
-  | otherwise = findRoot $ root a
+connect :: Tree a -> Tree a
 
-connected :: Node -> Node -> Node
-connected a b
-  | (numberOfLevel.root) a == (numberOfLevel.root) b = 
+main = do
+  let n0 = Node 0 []
+  let n1 = Node 1 []
+  let n2 = Node 2 []
+  let n3 = Node 3 []
+  let n4 = Node 4 []
+  let n5 = Node 5 []
+  let n6 = Node 6 []
+  let n7 = Node 7 []
+  let n8 = Node 8 []
+  let n9 = Node 9 []
+
+  connect
