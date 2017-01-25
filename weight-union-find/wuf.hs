2adbc7bedc63a736b9d37ad7716e432a2dd88e30
ccQpein <ccqpein@protonmail.com>
2017-01-25 10:19:16 -0500

    a bit

--- weight-union-find/wuf.hs
+++ weight-union-find/wuf.hs
@@ -1,17 +1,14 @@
 import           Data.Tree
 
-connect :: Tree a -> Tree a
+connect :: Tree a -> Tree a -> Tree a
+connect a b = Node (rootLabel a) (b:(subForest a))
 
 main = do
   let n0 = Node 0 []
   let n1 = Node 1 []
   let n2 = Node 2 []
-  let n3 = Node 3 []
-  let n4 = Node 4 []
-  let n5 = Node 5 []
-  let n6 = Node 6 []
-  let n7 = Node 7 []
-  let n8 = Node 8 []
-  let n9 = Node 9 []
 
-  connect
+  let n3 = connect n0 n1
+  let n4 = connect n3 n2
+
+  return n4
