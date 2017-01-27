d20e050911d711041443556a17b91b74897cbf78
ccQpein <ccqpein@protonmail.com>
2017-01-26 19:18:06 -0500

    add a bit

--- Depth-First-Search/Graph.swift
+++ Depth-First-Search/Graph.swift
@@ -1,13 +1,18 @@
 protocol Graph{
-    var pointSet:Set<Int> {set get}
-    var pathSet:Set<Set<Int>> {set get}
-    init (points:Set<Int>, paths:Set<Set<Int>>)
+    associatedtype ItemType:Hashable
+    var pointSet:ItemType {set get}
+    var pathSet:Set<ItemType> {set get}
+    init (V points:ItemType, E paths:Set<ItemType>)
 }
 
 class UndirGraph:Graph {
-    var pointSet:Set
-    var pathSet:Set<Set>
-    init(){
-        
+    typealias ItemType = Set<Int>
+
+    var pointSet:Set<Int>
+    var pathSet:Set<Set<Int>>
+
+    required init(V points:Set<Int>, E paths:Set<Set<Int>>){
+        self.pointSet = points
+        self.pathSet = paths
     }
 }
