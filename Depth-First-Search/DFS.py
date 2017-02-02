81ee69cd3e58845ba188ebe1bcee05ae2a1545a3
ccQpein <ccqpein@protonmail.com>
2017-02-02 15:35:11 -0500

    finish python version and add new topologicalSort function

--- Depth-First-Search/DFS.py
+++ Depth-First-Search/DFS.py
@@ -12,7 +12,7 @@ class Graph():
             return
 
         self.pointSet = points
-        self.pointSet.sort()
+        self.pointSet.sort()  # sort points to alphabetical
         self.pathDic = paths
 
         for i in self.pointSet:
@@ -21,8 +21,23 @@ class Graph():
             except KeyError:  # if some point has no path, give it a empty list
                 self.pathDic[i] = []
 
+            # init each points discovery and finishing time list
+            self.pointTime[i] = []
 
-# data test
+# test
+test1set = ["u", "v", "w", "x", "y", "z", ]
+test1path = {"u": ["x", "v"],
+             "v": ["y"],
+             "w": ["y", "z"],
+             "x": ["v"],
+             "y": ["x"],
+             "z": ["z"],
+             }
+
+# test1 = Graph(test1set, test1path)
+
+
+# q1 data
 q1set = ["s", "v", "w", "q", "t", "x", "z", "y", "r", "u", ]
 q1path = {"s": ["v"],
           "v": ["w"],
@@ -36,7 +51,70 @@ q1path = {"s": ["v"],
           "u": ["y"],
           }
 
-q1 = Graph(q1set, q1path)
+# q1 = Graph(q1set, q1path)
+
+
+# q2 data
+q2set = ["m", "n", "q", "o", "p", "r", "s",
+         "t", "u", "v", "w", "x", "y", "z", ]
+q2path = {"m": ["x", "q", "r", ],
+          "n": ["o", "q", "u", ],
+          "q": ["t", ],
+          "o": ["r", "s", "v", ],
+          "p": ["o", "s", "z", ],
+          "r": ["u", "y", ],
+          "s": ["r", ],
+          "t": [],
+          "u": ["t", ],
+          "v": ["x", "w", ],
+          "w": ["z", ],
+          "x": [],
+          "y": ["v", ],
+          "z": [],
+          }
+
+# q2 = Graph(q2set,q2path)
 
 
 def DFS(g):
+    time = 0
+    greyMark = set()
+    blackMark = set()
+
+    def DFS_inner(g, i, time):
+        time += 1
+        greyMark.add(i)
+        g.pointTime[i].append(time)
+        for c in g.pathDic[i]:
+            if c in blackMark or c in greyMark:
+                pass
+            else:
+                time = DFS_inner(g, c, time)
+        time += 1
+        g.pointTime[i].append(time)
+        blackMark.add(i)
+        greyMark.remove(i)
+        return time
+
+    for i in g.pointSet:
+        if i in blackMark or i in greyMark:
+            pass
+        else:
+            time = DFS_inner(g, i, time)
+
+    return g.pointTime
+
+
+def topologicalSort(g):
+    DFS(g)  # in case graph has not DFSed before
+    finishTimeList = []
+    for k, v in g.pointTime.items():
+        finishTimeList.append((k, v[1]))
+
+    finishTimeList.sort(key=lambda pair: pair[1])
+
+    result = []
+    for i in finishTimeList:
+        result.insert(0, i[0])
+
+    return result
