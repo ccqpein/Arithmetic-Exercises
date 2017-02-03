71dcdf1f63129aaec35e343d7ed1c393f2dfed3e
ccQpein <ccqpein@protonmail.com>
2017-02-03 16:05:03 -0500

    a bit

--- Depth-First-Search/DFS.py
+++ Depth-First-Search/DFS.py
@@ -1,8 +1,8 @@
 class Graph():
     pointSet = []
-    # it should be a dict on {point:[list of point it connect]}
+    # it should be a dict on {point:[list of point it connects]}
     pathDic = dict()
-    pointTime = dict()
+    pointTime = dict()  # points' discover time and finish time
 
     def __init__(self, points, paths):
         if set(paths.keys()) - set(points):
@@ -14,6 +14,8 @@ class Graph():
         self.pointSet = points
         self.pointSet.sort()  # sort points to alphabetical
         self.pathDic = paths
+        # init each points discovery and finishing time list
+        self.pointTime = {key: [] for key in self.pointSet}
 
         for i in self.pointSet:
             try:
@@ -21,8 +23,6 @@ class Graph():
             except KeyError:  # if some point has no path, give it a empty list
                 self.pathDic[i] = []
 
-            # init each points discovery and finishing time list
-            self.pointTime[i] = []
 
 # test
 test1set = ["u", "v", "w", "x", "y", "z", ]
@@ -78,22 +78,23 @@ q2path = {"m": ["x", "q", "r", ],
 
 def DFS(g):
     time = 0
-    greyMark = set()
-    blackMark = set()
+    greyMark = set()  # grey set for storing all points in recursive
+    blackMark = set()  # black set for storing all points have done
 
-    def DFS_inner(g, i, time):
+    def DFS_inner(g, i, time):  # recursive function
         time += 1
-        greyMark.add(i)
-        g.pointTime[i].append(time)
+        greyMark.add(i)  # add to grey set
+        g.pointTime[i].append(time)  # store discover time
         for c in g.pathDic[i]:
             if c in blackMark or c in greyMark:
                 pass
             else:
                 time = DFS_inner(g, c, time)
         time += 1
+        # store finish time, so finish time's index is 1
         g.pointTime[i].append(time)
-        blackMark.add(i)
-        greyMark.remove(i)
+        blackMark.add(i)  # finish
+        greyMark.remove(i)  # delete grey set
         return time
 
     for i in g.pointSet:
@@ -102,19 +103,42 @@ def DFS(g):
         else:
             time = DFS_inner(g, i, time)
 
-    return g.pointTime
+    # format print
+    for k in g.pointSet:
+        print("{0} -> discover time is {1} -> finish time is {2}"
+              .format(k, g.pointTime[k][0], g.pointTime[k][1]))
+    return "done"
 
 
 def topologicalSort(g):
     DFS(g)  # in case graph has not DFSed before
+
+    # create list of turtle that [(point, finish time)]
     finishTimeList = []
     for k, v in g.pointTime.items():
-        finishTimeList.append((k, v[1]))
+        finishTimeList.append((k, v[1]))  # v[1] is finish time
 
+    # sort elements increasing by finish time
     finishTimeList.sort(key=lambda pair: pair[1])
 
+    # insert on the front of result list
     result = []
     for i in finishTimeList:
         result.insert(0, i[0])
 
-    return result
+    # format print
+    reStr = result[0]
+    for i in result[1:]:
+        reStr += " -> " + i
+    print(reStr)
+    return "done"
+
+if __name__ == "__main__":
+    # test1 = Graph(test1set, test1path)
+    q1 = Graph(q1set, q1path)
+    q2 = Graph(q2set, q2path)
+
+    DFS(q1)
+    print("\n")
+    topologicalSort(q2)
+    print("\n")
