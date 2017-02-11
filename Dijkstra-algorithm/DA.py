849bd04f46b79c50beae394c215460f97d8ebe9a
ccQpein <ccqpein@protonmail.com>
2017-02-10 20:18:23 -0500

    a bit

--- Dijkstra-algorithm/DA.py
+++ Dijkstra-algorithm/DA.py
@@ -1,17 +1,19 @@
 class PathGraph():
     pointSet = []
-    pathLen = {}  # {point:{shortMark:Bool,p1:length,p2:length...},...}
+    pathLen = {}  # {point:{shortMark:Bool,val:{p1:length,p2:length...},...}}
 
     def __init__(self, points):
         self.pointSet = points
+        for p in self.pointSet:
+            self.pathLen[p] = {"shortMark": False, "val": {p: 0}}
 
     def add_path(self, start, to, length):
-        self.pathLen[start] = {to: length}
+        self.pathLen[start]["val"][to] = length
         self.pathLen[start]["shortest"] = False
 
     def path(self, start, to):
         try:
-            return self.pathLen[start][to]
+            return self.pathLen[start]["val"][to]
         except:
             print("No value")
             return None
@@ -29,3 +31,30 @@ class PathGraph():
         else:
             print("No value")
             return None
+
+
+def Dijkstra(g, start):
+    tempSet = set(g.pointSet)
+    tempSet.remove(start)
+
+    while len(tempSet) != 0:
+        for v in sorted(a.pathLen[start]["val"], key=lambda x: x[1]):
+            if v[0] in tempSet:
+                thisPoint = v[0]
+                break
+        for (k, v) in g.pathLen[thisPoint]["val"].items:
+
+            # if __name__ == "__main__":
+qset = ["s", "t", "y", "x", "z"]
+a = PathGraph(qset)
+
+a.add_path("s", "t", 10)
+a.add_path("s", "y", 5)
+a.add_path("t", "y", 2)
+a.add_path("t", "x", 1)
+a.add_path("x", "z", 4)
+a.add_path("y", "t", 3)
+a.add_path("y", "x", 9)
+a.add_path("y", "z", 2)
+a.add_path("z", "x", 6)
+a.add_path("z", "s", 7)
