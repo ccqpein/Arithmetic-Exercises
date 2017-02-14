a9331655629f119f622277be6c5c295dd9f9cfa2
ccQpein <ccqpein@protonmail.com>
2017-02-13 22:36:21 -0500

    finish

--- Dijkstra-algorithm/DA.py
+++ Dijkstra-algorithm/DA.py
@@ -1,15 +1,20 @@
 class PathGraph():
     pointSet = []
-    pathLen = {}  # {point:{shortMark:Bool,val:{p1:length,p2:length...},...}}
+    # {point:{shortMark:Bool,val:{targetPoint1:length,p2:length,..},..}}
+    pathLen = {}
+    pathList = {}  # {start:{end:[points],..},..}
 
     def __init__(self, points):
         self.pointSet = points
         for p in self.pointSet:
             self.pathLen[p] = {"shortMark": False, "val": {p: 0}}
+            self.pathList[p] = {p: [p]}
 
     def add_path(self, start, to, length):
         self.pathLen[start]["val"][to] = length
         self.pathLen[start]["shortest"] = False
+
+        self.pathList[start][to] = [start, to]
 
     def path(self, start, to=None):
         if not to:
@@ -18,7 +23,7 @@ class PathGraph():
         try:
             return self.pathLen[start]["val"][to]
         except:
-            print("No value")
+            # print("No value")
             return None
 
     def shortest(self, start, to):
@@ -40,30 +45,52 @@ def Dijkstra(g, start):
     tempSet = set(g.pointSet)
 
     while len(tempSet) != 0:
-        for v in sorted(a.path(start).items(), key=lambda x: x[1]):
+        for v in sorted(g.path(start).items(), key=lambda x: x[1]):
             if v[0] in tempSet:
                 thisPoint = v[0]
                 break
+        # print(thisPoint)
         for (k, v) in g.path(thisPoint).items():
             sumValue = g.path(start, thisPoint) + g.path(thisPoint, k)
             try:
                 if sumValue < g.path(start, k):
                     g.path(start)[k] = sumValue
+                    g.pathList[start][k] = g.pathList[start][thisPoint] + [k]
             except:
                 g.add_path(start, k, sumValue)
+                g.pathList[start][k] = g.pathList[start][thisPoint] + [k]
         tempSet.remove(thisPoint)
 
-# if __name__ == "__main__":
-qset = ["s", "t", "y", "x", "z"]
-a = PathGraph(qset)
+if __name__ == "__main__":
+    qset = ["s", "t", "y", "x", "z"]
 
-a.add_path("s", "t", 10)
-a.add_path("s", "y", 5)
-a.add_path("t", "y", 2)
-a.add_path("t", "x", 1)
-a.add_path("x", "z", 4)
-a.add_path("y", "t", 3)
-a.add_path("y", "x", 9)
-a.add_path("y", "z", 2)
-a.add_path("z", "x", 6)
-a.add_path("z", "s", 7)
+    a = PathGraph(qset)
+    a.add_path("s", "t", 10)
+    a.add_path("s", "y", 5)
+    a.add_path("t", "y", 2)
+    a.add_path("t", "x", 1)
+    a.add_path("x", "z", 4)
+    a.add_path("y", "t", 3)
+    a.add_path("y", "x", 9)
+    a.add_path("y", "z", 2)
+    a.add_path("z", "x", 6)
+    a.add_path("z", "s", 7)
+    Dijkstra(a, "s")
+    print(a.path("s"))
+    print(a.pathList["s"])
+
+    a = PathGraph(qset)
+    a.add_path("s", "t", 10)
+    a.add_path("s", "y", 5)
+    a.add_path("t", "y", 2)
+    a.add_path("t", "x", 1)
+    a.add_path("x", "z", 4)
+    a.add_path("y", "t", 3)
+    a.add_path("y", "x", 9)
+    a.add_path("y", "z", 2)
+    a.add_path("z", "x", 6)
+    a.add_path("z", "s", 7)
+
+    Dijkstra(a, "z")
+    print(a.path("z"))
+    print(a.pathList["z"])
