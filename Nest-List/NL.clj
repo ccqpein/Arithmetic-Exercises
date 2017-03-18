0e1c305a83cf5d86ad944686539846b554801003
ccQpein <ccqpein@protonmail.com>
2017-03-18 10:58:51 -0400

    finish clojure version

--- Nest-List/NL.clj
+++ Nest-List/NL.clj
@@ -1,9 +1,17 @@
 (def testList [1, [3, [2]], [[[5]]]])
 (def testList2 [1, [3, [2], [6, 7]], [[4, [5]]]])
 
-(defn NL [reLis lis]
-  (cond (empty? lis) reLis
-        (= (first lis) []) 
-        (instance? clojure.lang.PersistentVector (first lis))
-        )
-)
+(defn NL [lis]
+  (let [innerList (atom lis)
+        reLis (atom [])]
+    (while (not (empty? @innerList))
+      (let [firstE (first @innerList)]
+        (cond (= firstE [])
+              (reset! innerList (into [] (rest @innerList)))
+              (instance? java.lang.Long firstE)
+              (do (swap! reLis conj firstE)
+                  (reset! innerList (into [] (rest @innerList))))
+              :else
+              (reset! innerList (into [] (concat firstE (rest @innerList))))
+              )))
+    reLis))
