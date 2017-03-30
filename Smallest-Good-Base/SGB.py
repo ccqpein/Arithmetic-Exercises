c43de2e272e504334f728af5c0932f7dd5e66438
ccQpein <ccqpein@protonmail.com>
2017-03-30 16:26:25 -0400

    add new

---
+++ Smallest-Good-Base/SGB.py
@@ -0,0 +1,20 @@
+import math
+
+test1 = "13"
+test2 = "4681"
+test3 = "1000000000000000000"
+
+
+class Solution(object):
+
+    def smallestGoodBase(self, n):
+        """
+        :type n: str
+        :rtype: str
+        """
+        n = int(n)
+        for l in range(int(math.log(n, 2)) + 1, 2, -1):
+            k = int(math.pow(n, 1 / (l - 1)))
+            if (k ** l - 1) / (k - 1) == n:
+                return str(k)
+        return str(n - 1)
