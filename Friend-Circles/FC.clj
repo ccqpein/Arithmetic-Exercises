d60ed312542f88c9e08a4265230d1cab31e66b2b
ccQpein <ccqpein@protonmail.com>
2017-04-07 21:14:12 -0400

    a bit

--- Friend-Circles/FC.clj
+++ Friend-Circles/FC.clj
@@ -1,22 +1,32 @@
-(def test1 '((1 1 0)
-             (1 1 0)
-             (0 0 1))) ; 2
+(def test1 [[1 1 0]
+             [1 1 0]
+             [0 0 1]]) ; 2
 
-(def test2 '((1 1 0)
-             (1 1 1)
-             (0 1 1))) ; 1
+(def test2 [[1 1 0]
+             [1 1 1]
+             [0 1 1]]) ; 1
 
-(def test3 '((1 0 0 1)
-             (0 1 1 0)
-             (0 1 1 1)
-             (1 0 1 1))) ; 1
+(def test3 '[[1 0 0 1]
+             [0 1 1 0]
+             [0 1 1 1]
+             [1 0 1 1]]) ; 1
 
-(def test4 '((1 1 0)
-             (1 1 0)
-             (0 0 1))) ; 2
+(def test4 [[1 1 0]
+             [1 1 0]
+             [0 0 1]]) ; 2
 
-(defn friend-circle [m]
-  (let [total (range 0 (count m))]
-    (loop [numSet total]
-      (do )
-      )))
+
+(defn find-circle [m]
+  (let [totalNum (count m)
+        circle (atom 0)
+        pool (atom (set (range 0 totalNum)))]
+    (while (-> @pool empty? not)
+      (let [this (first @pool)]
+        ((fn [ind]
+           (loop [indIn ind]
+             (doseq [i (range 0 indIn)]
+               (if (and (-> m (nth indIn) (nth i) (= 1))
+                        (contains? @pool i))
+                 (do (swap! pool #(set (remove %)) #{i})
+                     (recur (first @pool))))))) this))
+      (swap! pool inc))))
