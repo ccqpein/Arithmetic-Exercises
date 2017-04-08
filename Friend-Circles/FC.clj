0917873520189e460f7daaab8ebabec8bb955006
ccQpein <ccqpein@protonmail.com>
2017-04-07 22:32:12 -0400

    nil

--- Friend-Circles/FC.clj
+++ Friend-Circles/FC.clj
@@ -16,17 +16,5 @@
              [0 0 1]]) ; 2
 
 
-(defn find-circle [m]
-  (let [totalNum (count m)
-        circle (atom 0)
-        pool (atom (set (range 0 totalNum)))]
-    (while (-> @pool empty? not)
-      (let [this (first @pool)]
-        ((fn [ind]
-           (loop [indIn ind]
-             (doseq [i (range 0 indIn)]
-               (if (and (-> m (nth indIn) (nth i) (= 1))
-                        (contains? @pool i))
-                 (do (swap! pool #(set (remove %)) #{i})
-                     (recur (first @pool))))))) this))
-      (swap! pool inc))))
+
+
