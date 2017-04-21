a042cec62135123ff7242b473bacd7eae3926d8f
ccQpein <ccqpein@protonmail.com>
2017-04-21 16:59:14 -0400

    finish haskell version

--- Bubble-Sort/BS.hs
+++ Bubble-Sort/BS.hs
@@ -1,10 +1,16 @@
-{--main = do
+main = do
   let testData = [14,33,27,35,10]
-  let flag = True
-  print (bubbleSort testData flag)
---}
-bubbleSort :: ([Integer],Bool) -> ([Integer],Bool)
-bubbleSort ((x:[]),a) = ([x],a)
-bubbleSort (y:x:xs)
-  | x < y = x:(bubbleSort (y:xs))
-  | otherwise = y:(bubbleSort (x:xs))
+  let flag = False
+  let sort a f =
+        let temp = bubbleSort a f in
+            if snd temp
+              then sort (fst temp) flag
+              else return (fst temp)
+  sort testData flag
+
+bubbleSort :: [Integer] -> Bool -> ([Integer],Bool)
+bubbleSort (x:[]) a = ([x],a)
+bubbleSort (y:x:xs) a
+  | x < y = (x:(fst $ bubbleSort (y:xs) True), True)
+  | otherwise = let childResult = bubbleSort (x:xs) a in
+                  (y:(fst $ childResult), (snd $ childResult))
