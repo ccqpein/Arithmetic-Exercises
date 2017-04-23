82d4fcfd8beb29c0b41b62830b7344ce975a9424
ccQpein <ccqpein@protonmail.com>
2017-04-23 11:01:48 -0400

    finish haskell version

--- Selection-Sort/SS.hs
+++ Selection-Sort/SS.hs
@@ -1,7 +1,16 @@
+import           Data.List (delete)
+-- import           Debug.Trace
+
 main = do
   let testDate = [14,33,27,10,35,19,42,44]
+  print (selectionSort testDate)
 
 
 selectionSort :: [Integer] -> [Integer]
 selectionSort [] = []
-selectionSort a = foldl 
+selectionSort a = let childResult = minimum' a in
+                       (fst childResult):(selectionSort $ snd childResult)
+
+minimum' :: [Integer] -> (Integer, [Integer])
+minimum' a = let minVal = minimum a in
+               (minVal, (delete minVal a))
