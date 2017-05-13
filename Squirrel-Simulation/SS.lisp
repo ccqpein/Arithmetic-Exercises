e336290a8f2920efbe13eff913972a67876de810
ccQpein <ccqpein@protonmail.com>
2017-05-12 23:05:51 -0400

    finish lisp version

--- Squirrel-Simulation/SS.lisp
+++ Squirrel-Simulation/SS.lisp
@@ -1,10 +1,15 @@
 (defun min-distance (tree squirrel nuts)
-  (let* ((dist (lambda (p1 p2)
-                 (loop for x in p1
-                    for y in p2
-                    sum (abs (- x y)))))
-         (minNutDist (- (dist (car nuts) squirrel)
-                        (dist (car nuts) tree)))
-         (nuts2tree 0))
-    (loop for nut in nuts
-         do ())))
+  (labels ((dist (p1 p2)
+             (loop for x in p1
+                for y in p2
+                sum (abs (- x y)))))
+    (let* ((minNutDist (- (dist (car nuts) squirrel)
+                          (dist (car nuts) tree))))
+      (+ (loop for nut in nuts
+            when (<= (- (dist nut squirrel)
+                        (dist nut tree)) minNutDist)
+            do (setf minNutDist
+                     (- (dist nut squirrel)
+                        (dist nut tree)))
+            sum (* 2 (dist nut tree))
+              ) minNutDist))))
