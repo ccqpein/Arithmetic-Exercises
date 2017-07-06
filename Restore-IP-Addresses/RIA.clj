603cfc9d0769c7cf9ca45dce94c985d9354f73e2
ccQpein <ccqpein@protonmail.com>
2017-07-06 14:51:15 -0400

    add a bit

--- Restore-IP-Addresses/RIA.clj
+++ Restore-IP-Addresses/RIA.clj
@@ -3,9 +3,28 @@
 (def testStr1 "010010")
 (def testStr1 "101023")
 
+(defn filter-ip [ip]
+  (cond (> (Integer. ip) 255) false
+        (and (> (count ip) 1) (= \0 (nth ip 0))) false
+        :else
+        true)))
+
+(defn find-next [ipstr doneip]
+  "give total ip string and ip done, return next possible ip"
+  (let [allpossible (for [i (range 1 4)
+                          :let [len (count doneip)]]
+                      (subs ipstr len (+ len i)))]
+    (for [ip allpossible
+          :when (filter-ip ip)]
+      ip)))
+
+
 (defn restore-Ip-Addresses [ipstr]
   (let [ippool (atom [])]))
 
-(defn find-next [ipstr ipdonelist]
-  "give total ip string and ip done list, return next possible ip"
-  )
+
+
+
+
+
+
