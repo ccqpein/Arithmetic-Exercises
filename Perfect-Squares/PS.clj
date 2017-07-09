e02442acc8821301cb917c88bebc5651e68ee1f8
ccQpein <ccqpein@protonmail.com>
2017-07-09 13:27:05 -0400

    finish clojure version

--- Perfect-Squares/PS.clj
+++ Perfect-Squares/PS.clj
@@ -1,7 +1,23 @@
 ;;; Using the better solution in README
+;;; A lot side effect operation LOL
 
 (defn numSquares [num]
-  (if (< num 2) num)
   (let [lst (atom #{})
-        toCheck (atom #{})]
-    ()))
+        toCheck (atom #{num})
+        i (atom 0)]
+    (while (< (* @i @i) num)
+      (do (swap! i inc)
+          (swap! lst conj (* @i @i))))
+    ;(println @lst)
+    (loop [Check @toCheck
+           cnt 1]
+      ;(println Check)      
+      (cond (or (contains? Check 0) (empty? Check)) 0
+            (->> (clojure.set/intersection Check @lst) empty? not) cnt
+            :else
+            (recur (set (for [x Check y @lst :when (< y x)]
+                          (- x y)))
+                   (inc cnt))))))
+
+(numSquares 12) ;3 => 4 + 4 + 4
+(numSquares 13) ;2 => 4 + 9
