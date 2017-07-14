22d6eac803a49ed6e03f3aac6c9100aeac04ea70
ccQpein <ccqpein@protonmail.com>
2017-07-14 10:58:48 -0400

    finish go version

--- Kill-Process/KP.go
+++ Kill-Process/KP.go
@@ -17,9 +17,23 @@ func killProcess(pid, ppid []int, kill int) []int {
 
 	//Println(tempDic)
 
-	return []int{1, 2, 3}
+	rest, stack := []int{}, []int{kill}
+
+	for len(stack) > 0 {
+		tempStack := []int{}
+		for _, p := range stack {
+			rest = append(rest, p)
+			for _, pp := range tempDic[p] {
+				tempStack = append(tempStack, pp)
+			}
+		}
+		stack = tempStack
+	}
+
+	return rest
 }
 
 func main() {
 	Println(killProcess([]int{1, 3, 10, 5}, []int{3, 0, 5, 3}, 5))
+	Println(killProcess([]int{1, 3, 10, 5}, []int{3, 0, 5, 3}, 3))
 }
