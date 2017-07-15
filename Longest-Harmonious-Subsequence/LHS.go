53841f01c8b00771dfda215b2d398416608873e9
ccQpein <ccqpein@protonmail.com>
2017-07-15 14:05:44 -0400

    finish and review golang

---
+++ Longest-Harmonious-Subsequence/LHS.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	. "fmt"
+	"sort"
+)
+
+func findLHS(nums []int) int {
+	dic := map[int]int{}
+	for _, v := range nums {
+		if va, ok := dic[v]; ok {
+			dic[v] = 1 + va
+		} else {
+			dic[v] = 1
+		}
+	}
+
+	sortkey := []int{}
+	for k := range dic {
+		sortkey = append(sortkey, k)
+	}
+	sort.Ints(sortkey)
+
+	result := 0
+	for ind := 1; ind < len(sortkey); ind++ {
+		if temp := dic[sortkey[ind]] + dic[sortkey[ind-1]]; sortkey[ind]-sortkey[ind-1] == 1 &&
+			temp > result {
+			result = temp
+		}
+	}
+
+	return result
+}
+
+func main() {
+	Println(findLHS([]int{1, 3, 2, 2, 5, 2, 3, 7}))
+
+}
