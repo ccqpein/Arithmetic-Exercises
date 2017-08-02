88f53ed3d652a917f4a2b9b7f07d1aa1fbe1cfa4
ccQpein <ccqpein@protonmail.com>
2017-08-02 10:31:39 -0400

    finish scala version

--- Replace-Words/RW.scala
+++ Replace-Words/RW.scala
@@ -1,3 +1,18 @@
 def replaceWords(dict:Array[String], sentence:String):String = {
-
+  var tempList = Array[String]()
+  var sentList = sentence.split(" ")
+  for (keyword <- dict) {
+    tempList = Array()
+    for (word <- sentList) {
+      if ((keyword.length <= word.length) && (keyword == word.slice(0,keyword.length))){
+        tempList = tempList :+ keyword
+      }else {
+        tempList = tempList :+ word
+      }
+    }
+    sentList = tempList
+  }
+  sentList.mkString(" ")
 }
+
+println(replaceWords(Array("cat", "bat", "rat"), "the cattle was rattled by the battery"))
