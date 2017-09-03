4ffc762183030e6af6c97769fc86786782abd7c1
ccQpein <ccqpein@protonmail.com>
2017-09-03 19:15:16 -0400

    using haskell complex number packages

--- Complex-Number-Multiplication/CNM.hs
+++ Complex-Number-Multiplication/CNM.hs
@@ -1,5 +1,20 @@
+{-- --:= Mark: find better way to construct complex number
 type ImaginaryUnit = Integer
 type RealNumber = Integer
 data ComplexNumber = ComplexN {real :: RealNumber,
                                imag :: ImaginaryUnit}
+--}
 
+import           Data.Complex
+
+readComplex :: String -> Complex Double
+readComplex = read
+
+result :: (String, String) -> Complex Double
+result (x,y) = (readComplex x) * (readComplex y)
+
+main = do
+  let exm1 = ("1 :+ 1","1 :+ 1")
+  let exm2 = ("1 :+ -1","1 :+ -1")
+  print $ result exm1
+  print $ result exm2
