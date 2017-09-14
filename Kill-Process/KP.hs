d77e524353fca7a8fa24cca2472b2907f7a5fdc6
ccQpein <ccqpein@protonmail.com>
2017-09-13 20:13:56 -0400

    a bit

--- Kill-Process/KP.hs
+++ Kill-Process/KP.hs
@@ -10,19 +10,26 @@ makeMap ppid pid
         | otherwise = insertPMap (Map.insertWith (++) (pp!!i) [p!!i] m) (1 + i) pp p
 
 
-
 killProcess :: (Ord a, Num a) => Map.Map a [a] -> [a] -> [a]
 -- I dont know whether Map.empty always be matched
--- killProcess empty _  = [100]
+-- https://stackoverflow.com/questions/46144610/haskell-pattern-matching-match-map-empty-any-with-any-map-map-k-v
 killProcess _ []     = []
 killProcess m (x:xs)
+  | Map.null m = []
   | m Map.!? x == Nothing = (killProcess m xs) ++ [x]
   | otherwise = (killProcess m ((m Map.! x) ++ xs)) ++ [x]
+
+-- Test pattern matching of constructor.
+data Foo = Bar | Baz Int
+f :: Foo -> Int
+f Bar     = 1
+f (Baz x) = x - 1
 
 main = do
   let dict = makeMap [3,0,5,3] [1,3,10,5]
   print dict
   -- := Bug next, give 2 which not including in ppid or pid, will return [2]
+  print $ killProcess Map.empty [1,2,3]
   print $ killProcess dict [2]
   print $ killProcess dict [3]
   print $ killProcess dict [0]
