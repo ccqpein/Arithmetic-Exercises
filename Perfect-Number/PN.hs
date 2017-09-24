db863276619878487a4133dabae8323c231dc852
ccQpein <ccqpein@protonmail.com>
2017-09-24 17:23:04 -0400

    finish

--- Perfect-Number/PN.hs
+++ Perfect-Number/PN.hs
@@ -1,1 +1,12 @@
-checkPerfectNumber 
+checkPerfectNumber :: Int -> Bool
+checkPerfectNumber a
+  | a <= 1 = False
+  | a * 2== foldr (\x z -> if mod a x == 0
+                 then z + x + a `div` x
+                 else z) 0
+    [1..edge a] = True
+  | otherwise = False
+
+
+edge :: Int -> Int
+edge a = round (sqrt (fromIntegral a) :: Double) :: Int
