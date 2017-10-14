0620a98fe02bb32e57c2deee23ced34885817d33
ccQpein <ccqpein@protonmail.com>
2017-10-14 10:34:57 -0400

    fix

--- Binary-Search/bs.hs
+++ Binary-Search/bs.hs
@@ -4,11 +4,12 @@ cutList x = splitAt (div (length x) 2) x
 binarySearch :: (Ord a, Num a) => [a] -> a -> Bool
 binarySearch [] _ = False
 binarySearch ls a
-  | head ls == a = True
+  | length ls == 1 && head ls == a = True
+  | length ls == 1 = False
   | otherwise =
     let (x,y) = cutList ls in
       if head y > a then binarySearch x a
-      else binarySearch x a
+      else binarySearch y a
 
 main = do
   let test = [10,14,19,26,27,31,33,35,42,44]
