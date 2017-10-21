3ee0787862508ab6fe576f3757e9ce7f448d06eb
ccQpein <ccqpein@protonmail.com>
2017-10-21 11:28:25 -0400

    want to change logic

--- Degree-of-an-Array/DoaA.clj
+++ Degree-of-an-Array/DoaA.clj
@@ -1,1 +1,17 @@
-(defn find-shortest-subarray [])
+(defn find-shortest-subarray [nums]
+  (loop [store-all-ele {}
+         stack-of-degree [0 []]
+         nums nums
+         ind 0]
+    (cond (empty? nums)
+          (apply min
+                 (map #(- (last (last (get store-all-ele %)))
+                          (first (last (get store-all-ele %))))
+                      ))
+          :else
+          (recur (try
+                   (if (> (count (last (get store-all-ele (first nums)))) 1)
+                     (update store-all-ele (first nums)
+                             (conj (last (get store-all-ele (first nums))) ind))))))))
+
+(defn update-store)
