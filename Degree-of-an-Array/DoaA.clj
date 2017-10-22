d07cbff510f02e84847ad1651d7265ff2557f4c0
ccQpein <ccqpein@protonmail.com>
2017-10-22 10:43:58 -0400

    finish

--- Degree-of-an-Array/DoaA.clj
+++ Degree-of-an-Array/DoaA.clj
@@ -1,17 +1,39 @@
-(defn find-shortest-subarray [nums]
-  (loop [store-all-ele {}
-         stack-of-degree [0 []]
-         nums nums
-         ind 0]
-    (cond (empty? nums)
-          (apply min
-                 (map #(- (last (last (get store-all-ele %)))
-                          (first (last (get store-all-ele %))))
-                      ))
-          :else
-          (recur (try
-                   (if (> (count (last (get store-all-ele (first nums)))) 1)
-                     (update store-all-ele (first nums)
-                             (conj (last (get store-all-ele (first nums))) ind))))))))
+(defn get-degree-nums [dict]
+  (let [largest (atom 0)
+        num-list (atom [])]
+    (doseq [key-value dict]
+      (cond (> (val key-value) @largest)
+            (do (reset! largest (val key-value))
+                (reset! num-list [(key key-value)]))
+            (= (val key-value) @largest)
+            (swap! num-list conj (key key-value))))
+    @num-list))
 
-(defn update-store)
+
+(defn find-shortest-subarray [nums]
+  (let [degree-num-dict (frequencies nums)
+        degree-num (get-degree-nums degree-num-dict)
+        store-index (atom (zipmap degree-num (repeat [])))]
+    (loop [ind 0
+           nums nums]
+      (cond (empty? nums)
+            (apply min
+                   (map #(- (last (last %)) (first (last %)) -1)
+                                          (into [] @store-index)))
+            :else
+            (do (if (some #(= (first nums) %) degree-num)
+                  (cond (> (count (get @store-index (first nums))) 1)
+                        (swap! store-index update-in [(first nums)]
+                               #(conj (into [] (butlast %)) ind))
+                        (< (count (get @store-index (first nums))) 1)
+                        (swap! store-index assoc (first nums) [ind])
+                        :else
+                        (swap! store-index update-in [(first nums)]
+                               #(conj % ind))))
+                ;(println store-index)
+                (recur (inc ind) (rest nums)))
+            ))))
+
+(find-shortest-subarray [1 2 2 3 1])
+(find-shortest-subarray [1,2,2,3,1,4,2])
+
--- Degree-of-an-Array/README.md
+++ Degree-of-an-Array/README.md
@@ -1,5 +1,7 @@
 # README #
 
+[Source](https://leetcode.com/problems/degree-of-an-array/description/)
+
 Given a non-empty array of non-negative integers nums, the degree of this array is defined as the maximum frequency of any one of its elements.
 
 Your task is to find the smallest possible length of a (contiguous) subarray of nums, that has the same degree as nums.
