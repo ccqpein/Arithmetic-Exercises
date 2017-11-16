097dbb0535190a5ed22f0f3ea0dd5ee3e0cdb19c
ccQpein <ccqpein@protonmail.com>
2017-11-15 21:25:48 -0500

    start try cpp

---
+++ Bubble-Sort/BS.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int bubbleSort(){
+  int arr[3] = {1,2,3};
+  return arr;
+}
+
+int main(){
+  int arr = bubbleSort();
+  for (int i = 0; i <3; i++){
+    cout << arr[i] << endl;
+  }
+  return 0;
+}
