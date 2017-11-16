16473e6b19f15396f3881d9f003fcd1403d91eb9
ccQpein <ccqpein@protonmail.com>
2017-11-16 16:12:53 -0500

    learn new stuff

--- Bubble-Sort/BS.cpp
+++ Bubble-Sort/BS.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
+#include <array>
 using namespace std;
 
-int bubbleSort(){
-  int arr[3] = {1,2,3};
+/* // code below is learning `dangling pointer`
+int* bubbleSort(){
+  int * arr = new int[3]; // avoid dangling pointer
+  for (int i =0 ; i <3; i++) arr[i] = i+1;
   return arr;
 }
 
 int main(){
-  int arr = bubbleSort();
+  int* arr = bubbleSort();
   for (int i = 0; i <3; i++){
-    cout << arr[i] << endl;
+    cout << *(arr+i) << endl;
   }
+  delete [] arr;
   return 0;
+}
+*/
+
+int main(){
+  array<int,3> arr = {1,2,3};
+  cout << arr[1] <<endl;
 }
