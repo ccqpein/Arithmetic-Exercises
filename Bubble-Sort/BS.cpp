5d280ea2bcd3bd0269bf5525a698e3d88a35cd90
ccQpein <ccqpein@protonmail.com>
2017-11-17 11:08:50 -0500

    implement list

--- Bubble-Sort/BS.cpp
+++ Bubble-Sort/BS.cpp
@@ -19,7 +19,60 @@ int main(){
 }
 */
 
+struct node {
+  int data;
+  node * next;
+};
+
+class list {
+private:
+  node * head, * tail;
+
+  node * createnode(int i){
+    node * temp = new node;
+    temp->data = i;
+    temp->next = NULL;
+
+    return temp;
+  };
+
+  
+public:
+  list() { // construct function
+    head = NULL;
+  }
+
+  list(int i,...) {
+    //https://www.cprogramming.com/tutorial/lesson17.html
+    va_list args;
+    va_start (args,i);
+    
+    int s = sizeof(args)/sizeof(long); // it should be long type
+
+    node * first = new node;
+    first->data = i;
+    first->next = NULL;
+    head = first;
+
+    node * temp = head;
+    
+    for (int ind = 0; ind < s; ind++) {
+      temp->next = createnode(va_arg(args,int));
+      temp = temp->next;
+    };
+  };
+
+  int show() {
+    node * temp = this->head;
+    do{
+      cout << temp->data << endl;
+      temp = temp->next;
+    }while(temp != NULL);
+  };
+  
+};
+
 int main(){
-  array<int,3> arr = {1,2,3};
-  cout << arr[1] <<endl;
+  list a  = list(1,2,3,4);
+  a.show();
 }
