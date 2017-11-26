4c48949220e517493f8b3e50ecf4bea3e59cd53e
ccQpein <ccqpein@protonmail.com>
2017-11-25 19:41:38 -0500

    a bit

--- Add-Strings/AS.clj
+++ Add-Strings/AS.clj
@@ -1,3 +1,5 @@
-(def chars [#\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9 #\0])
+(def chars [\1 \2 \3 \4 \5 \6 \7 \8 \9 \0])
 (def ints [1 2 3 4 5 6 7 8 9 0])
-(def dict {})
+(def dict (merge (zipmap chars ints)
+                 (zipmap ints chars)))
+
