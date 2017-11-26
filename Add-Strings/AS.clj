68ece409bc812808fd5dab17d6afb8c9bedd1e64
ccQpein <ccqpein@protonmail.com>
2017-11-26 11:33:46 -0500

    finish

--- Add-Strings/AS.clj
+++ Add-Strings/AS.clj
@@ -3,3 +3,28 @@
 (def dict (merge (zipmap chars ints)
                  (zipmap ints chars)))
 
+(defn add-strings [& nums]
+  (print nums)
+  (num-to-string (apply + (map string-to-num nums))))
+
+(defn string-to-num [^String str]
+  (loop [strl (seq str)
+         len (count strl)
+         ind 0
+         sum 0]
+    ;(println strl sum)
+    (cond (empty? strl) sum
+          :eles
+          (recur (rest strl)
+                 len
+                 (inc ind)
+                 (+ sum (* (get dict (first strl))
+                           (loop [count (- len ind 1)
+                                  this 1]
+                             (cond (= count 0) this
+                                   :else
+                                   (recur (- count 1) (* this 10))))))))))
+
+(defn num-to-string [^Integer int]
+  (str int))
+
