99c2fe64850f7bd1a80a4a897db98718d980ee9e
ccQpein <ccqpein@protonmail.com>
2017-12-02 19:14:05 -0500

    finish

--- Add-Strings/AS.swift
+++ Add-Strings/AS.swift
@@ -7,3 +7,42 @@ var dict1 = {() -> [Character:Int] in
     }
     return dictInner
 }()
+
+var dict2 = {() -> [Int:Character] in
+    var dictInner = Dictionary<Int,Character>()
+    var seed = 0
+    for c in "0123456789" {
+        dictInner[seed] = c
+        seed += 1
+    }
+    return dictInner
+}()
+
+
+func addString(numS1:String, numS2:String) -> String{
+    var sum = 0
+    for numS in [numS1,numS2]{
+        for ind in 0..<numS.count {
+            sum += (dict1[Array(numS)[ind]]! * {Int -> Int in
+                        var base = 1
+                        for _ in 0..<(numS.count-1-ind){
+                            base *= 10
+                        }
+                        return base
+                    }(ind))
+        }
+    }
+
+    var reStr = ""
+    while sum >= 10 {
+        reStr = (String(dict2[sum%10]!) + reStr)
+        sum = sum / 10
+    }
+
+
+    return (String(dict2[sum]!) + reStr)
+}
+
+
+addString(numS1:"0",numS2:"0")
+addString(numS1:"99",numS2:"9")
