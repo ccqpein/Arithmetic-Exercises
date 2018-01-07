cc267b3296c7c6c37d2bcda0e74bd0404e654cb6
ccQpein <ccqpein@protonmail.com>
2018-01-07 13:01:00 -0500

    finish rust version

--- Add-Digits/ad.rs
+++ Add-Digits/ad.rs
@@ -1,7 +1,12 @@
-fn ad(int i) {
-
+fn ad(i:u32) -> u32{
+    let mut inner_int = i;
+    while inner_int >= 10 {
+        inner_int = (inner_int/10) + (inner_int%10);
+    }
+    return inner_int;
 }
 
 fn main() {
-   
+    println!("{}",ad(10));
+    println!("{}",ad(38));
 }
