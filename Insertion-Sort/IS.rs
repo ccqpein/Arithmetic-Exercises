b2ed3dad48998df62650927920ba76e4ba377d6d
ccQpein <ccqpein@protonmail.com>
2018-01-28 11:06:13 -0500

    finish

--- Insertion-Sort/IS.rs
+++ Insertion-Sort/IS.rs
@@ -1,9 +1,14 @@
-fn insertiong_sort(nl: Vec<i32>, f: &Fn(&i32, &i32) -> bool) -> Vec<i32> {
+fn insertiong_sort(nl: &Vec<i32>, f: &Fn(&i32, &i32) -> bool) -> Vec<i32> {
     let mut head = vec![nl[0]];
     for i in &nl[1..] {
-        let thisInd = head.iter().position(|&r| f(r,i)).unwrap();
-        if thisInd 
+        if let Some(thisInd) = head.iter().position(|&r| f(&r, i)) {
+            head.insert(thisInd, *i);
+        } else {
+            head.push(*i);
+        }
+        println!("{:?}", head);
     }
+    return head;
 }
 
 fn large(a: &i32, b: &i32) -> bool {
@@ -12,4 +17,11 @@ fn large(a: &i32, b: &i32) -> bool {
 
 fn small(a: &i32, b: &i32) -> bool {
     return a < b;
+}
+
+
+fn main() {
+    let test = vec![3, 7, 4, 9, 5, 2, 6, 1];
+    insertiong_sort(&test, &large);
+    insertiong_sort(&test, &small);
 }
