c50d7a38149eb970d7080f12be1d8f0bf6e5e293
ccQpein <ccqpein@protonmail.com>
2018-02-23 14:08:18 -0500

    get problem

--- Round-Matrix/RM.rs
+++ Round-Matrix/RM.rs
@@ -2,13 +2,6 @@ fn get_value(a: &usize, b: &usize, m: &Vec<Vec<u32>>) -> u32 {
     m[*a][*b]
 }
 
-enum Direction {
-    Left,
-    Right,
-    Up,
-    Down,
-}
-
 fn round_matrix(m: &Vec<Vec<u32>>) -> Vec<u32> {
     let length = m.len();
     let mut direction = 0;
@@ -63,9 +56,93 @@ fn round_matrix(m: &Vec<Vec<u32>>) -> Vec<u32> {
     resutl
 }
 
-fn round_matrix2(m: &Vec<Vec<u32>>) -> Vec<u32> {}
+/*
+enum Direction<F>
+where
+    F: FnMut(&mut usize, &mut usize, &Vec<Vec<u32>>, &usize) -> u32,
+{
+    Left(F),
+    Right(F),
+    Up(F),
+    Down(F),
+}
+
+fn UpFn(a: &mut usize, b: &mut usize, m: &Vec<Vec<u32>>, flag: &usize) -> u32 {
+    let result = get_value(a, b, m);
+    if *a != 0 {
+        *a -= 1;
+    }
+    result
+}
+
+fn DownFn(a: &mut usize, b: &mut usize, m: &Vec<Vec<u32>>, flag: &usize) -> u32 {
+    let result = get_value(a, b, m);
+    if a != flag {
+        *a += 1;
+    }
+    result
+}
+
+fn LeftFn(a: &mut usize, b: &mut usize, m: &Vec<Vec<u32>>, flag: &usize) -> u32 {
+    let result = get_value(a, b, m);
+    if *b != 0 {
+        *b -= 1;
+    }
+    result
+}
+fn RightFn(a: &mut usize, b: &mut usize, m: &Vec<Vec<u32>>, flag: &usize) -> u32 {
+    let result = get_value(a, b, m);
+    if b != flag {
+        *b += 1;
+    }
+    result
+}
+
+fn round_matrix2(m: &Vec<Vec<u32>>) -> Vec<u32> {
+    let length = m.len();
+    let mut direction = Direction::Right(RightFn);
+    let mut a: usize = 0;
+    let mut b: usize = 0;
+
+    let mut resutl = vec![];
+
+    while a != 0 || b != 0 {
+        match direction {
+            Direction::Right(f) => {
+                resutl.push(f(&mut a, &mut b, m, &length));
+                if b == length {
+                    direction = Direction::Down(DownFn);
+                    b -= 1;
+                }
+            }
+            Direction::Down(f) => {
+                resutl.push(f(&mut a, &mut b, m, &length));
+                if b == length {
+                    direction = Direction::Left(LeftFn);
+                    b -= 1;
+                }
+            }
+            Direction::Left(f) => {
+                resutl.push(f(&mut a, &mut b, m, &length));
+                if b == length {
+                    direction = Direction::Up(UpFn);
+                    b -= 1;
+                }
+            }
+            Direction::Up(f) => {
+                resutl.push(f(&mut a, &mut b, m, &length));
+                if b == length {
+                    break;
+                }
+            }
+        }
+    }
+    resutl
+}
+*/
 
 fn main() {
     let testa = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
     println!("{:?}", round_matrix(&testa));
+    //println!("{:?}", round_matrix2(&testa));
 }
