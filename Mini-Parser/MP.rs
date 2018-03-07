b1aba257d94207b95e52c55a583340cc3b6dcd91
ccQpein <ccqpein@protonmail.com>
2018-03-06 22:05:06 -0500

    I think I could use test format

--- Mini-Parser/MP.rs
+++ Mini-Parser/MP.rs
@@ -1,1 +1,11 @@
-fn main() {}
+use std::str::Split;
+
+fn init_split(s: &str) -> Vec<&str> {
+    let result = s.split(",").collect();
+    result
+}
+
+fn main() {
+    let test1 = "111,222";
+    println!("{:?}", init_split(&test1));
+}
