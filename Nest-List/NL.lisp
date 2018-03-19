2624522aec053b7ba911a24fecc1621307d18ca4
ccQpein <ccqpein@protonmail.com>
2018-03-19 14:02:09 -0400

    add second function

--- Nest-List/NL.lisp
+++ Nest-List/NL.lisp
@@ -7,5 +7,16 @@
 	(t
 	 (append (list (car l)) (compress (cdr l))))))
 
+;; or you can use cons when (car l) is a element
+(defun compress2 (l)
+  (cond ((eql nil l) '())
+	((listp (car l))
+	 (append (compress (car l))
+		 (compress (cdr l))))
+	(t
+	 (cons (car l) (compress (cdr l)))))
+  )
+
 (print (compress '(1 (2 3 (4 5) (6 7 (8)) 9) 10)))
+(print (compress2 '(1 (2 3 (4 5) (6 7 (8)) 9) 10)))
 
