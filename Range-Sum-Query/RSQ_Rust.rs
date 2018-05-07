569300eb9efd02bba621971fc7fce6b9fc897c07
ccQpein <ccqpein@protonmail.com>
2018-05-07 16:42:16 -0400

    learn how to resolve Associated types vs type parameter conflict

--- Range-Sum-Query/RSQ_Rust.rs
+++ Range-Sum-Query/RSQ_Rust.rs
@@ -1,4 +1,5 @@
 use std::io::{Error, ErrorKind};
+use std::ops::Add;
 use std::result::Result;
 
 fn range_vec<T>(nl: &Vec<T>, i: usize, j: usize) -> Result<Vec<T>, Error>
@@ -33,10 +34,35 @@ where
     Result::Ok(result)
 }
 
+fn sum_vec<T>(nl: &Vec<T>) -> T
+where
+    T: Add<Output = T> + Copy + Clone,
+{
+    let mut result: T = *nl.first().unwrap();
+    if let Some((_, last)) = nl.split_first() {
+        for this in last {
+            result = result + (*this);
+        }
+    }
+    result
+}
+
+/*
+fn sum_range<T>(nl: &Vec<T>, i: usize, j: usize) -> Result<T, Error>
+where
+    T: Add + Copy + Clone,
+{
+
+}*/
+
 fn main() {
     let a = vec![1, 2, 3, 4, 5, 6];
     println!("{:?}", range_vec(&a, 0, 1));
     println!("{:?}", range_vec(&a, 2, 6));
     println!("{:?}", range_vec(&a, 6, 6));
     println!("{:?}", range_vec(&a, 5, 6));
+    //main thread cannot use `?`
+    //println!("{:?}", range_vec(&a, 2, 6)?);
+
+    println!("{:?}", sum_vec(&a));
 }
