d219acf5cf696d04f12641000f54975d4c304479
ccQpein <ccqpein@protonmail.com>
2018-06-02 16:17:10 -0400

    finish

--- Add-Two-Numbers/ATN.clj
+++ Add-Two-Numbers/ATN.clj
@@ -13,3 +13,21 @@
           (recur (into [] (rest a))
                  (into [] (rest b))
                  (conj re (+ (first a) (first b)))))))
+
+(defn check-ten [l]
+  (let [f (atom 0)
+        ll (doall (for [a l]
+                    (let [inner-a (+ a @f)]
+                      (if (>= inner-a 10)
+                        (do (reset! f (int (Math/floor (/ inner-a 10))))
+                            (mod inner-a 10))
+                        inner-a
+                        ))))]
+    (if (not= 0 @f)
+      (conj (into [] ll) @f)
+      (into [] ll))))
+
+(add-two-number [2 6 8] [8 9 3 1])
+(check-ten [10 15 11 1])
+
+(check-ten (add-two-number [9 9 9 9] [9 9 9 9]))
