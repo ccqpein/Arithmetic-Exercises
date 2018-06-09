fd36e75cd5ecbdd93e14e8715651aab781b0d099
ccQpein <ccqpein@protonmail.com>
2018-06-09 11:51:52 -0400

    finish

--- Replace-Words/RW.rs
+++ Replace-Words/RW.rs
@@ -1,8 +1,17 @@
-/*fn replace_words<'a>(words: &'a Vec<&'a str>, sentence: &'a str) -> Vec<String> {
-    let temp = sentence.split(' ').collect();
-    let mut result: Vec<String>;
-    for w in temp {}
-}*/
+fn replace_words<'a>(words: &'a Vec<&'a str>, sentence: &'a str) -> Vec<String> {
+    let temp: Vec<&str> = sentence.split(' ').collect();
+    let mut result: Vec<String> = vec![];
+    'outer: for w_in_sentence in temp {
+        for word in words {
+            if match_head_of_str(word, w_in_sentence) {
+                result.push(word.to_string());
+                continue 'outer;
+            }
+        }
+        result.push(w_in_sentence.to_string());
+    }
+    result
+}
 
 fn match_head_of_str(a: &str, b: &str) -> bool {
     let len1 = a.chars().count();
@@ -18,6 +27,6 @@ fn match_head_of_str(a: &str, b: &str) -> bool {
 fn main() {
     let words = vec!["cat", "bat", "rat"];
     let sentence = "the cattle was rattled by the battery";
-    //println!("{:?}", replace_words(&words, &sentence));
-    println!("{:?}", match_head_of_str("aa", "aaab"));
+    //println!("{:?}", match_head_of_str("aa", "aaab"));
+    println!("{:?}", replace_words(&words, &sentence));
 }
