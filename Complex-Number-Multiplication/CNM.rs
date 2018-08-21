c7b45e42daead0eacbeeb5617d701852028d1d85
ccQpein <ccqpein@protonmail.com>
2018-08-20 21:59:01 -0400

    a bit

--- Complex-Number-Multiplication/CNM.rs
+++ Complex-Number-Multiplication/CNM.rs
@@ -1,3 +1,18 @@
-fn complex_number_multiply<'a>(x: &'a str, y: &'a str) -> String {}
+fn complex_number_multiply<'a>(x: &'a str, y: &'a str) -> String {
+    let mut x_temp: Vec<&str> = x.split('+').collect();
+    let mut y_temp: Vec<&str> = y.split('+').collect();
 
-fn main() {}
+    println!("{:?}", x_temp);
+    println!("{:?}", y_temp);
+    println!("{:?}", y_temp[1].as_bytes()[1]);
+
+    let ins = x_temp[0].parse::<u32>().unwrap() * y_temp[0].parse::<u32>().unwrap();
+    //let com = y_temp
+    println!("{:?}", ins);
+
+    return String::from("aa");
+}
+
+fn main() {
+    complex_number_multiply("2+1i", "3+1i");
+}
