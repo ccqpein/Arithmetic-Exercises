9fff7ddc4d071d6b6eb485145e741bcb12e32e31
ccQpein <ccqpein@protonmail.com>
2018-08-25 10:27:55 -0400

    a bit

--- Elimination-Game/EG.rs
+++ Elimination-Game/EG.rs
@@ -1,5 +1,12 @@
 fn filter_sequence(l: Vec<u32>) -> Vec<u32> {
-    vec![]
+    let length = l.len();
+    let mut re: Vec<u32> = vec![];
+    for i in (1..length - 1).step_by(2) {
+        re.push(l[i]);
+    }
+    re
 }
 
-fn main() {}
+fn main() {
+    println!("{:?}", filter_sequence(vec![1, 2, 3, 4, 5, 6, 7]));
+}
