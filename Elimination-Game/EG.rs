82d30af5ec1761426357a6665b57a413dda77ef4
ccQpein <ccqpein@protonmail.com>
2018-08-27 14:11:58 -0400

    finish

--- Elimination-Game/EG.rs
+++ Elimination-Game/EG.rs
@@ -1,18 +1,19 @@
 fn filter_sequence(l: Vec<u32>) -> Vec<u32> {
     let length = l.len();
     let mut re: Vec<u32> = vec![];
-    for i in (1..length - 1).step_by(2) {
+    for i in (1..length).step_by(2) {
         re.push(l[i]);
     }
     re
 }
 
 fn last_remaining(n: u32) -> u32 {
-    let a: Vec<u32> = (1..n).collect();
+    let a: Vec<u32> = (1..n + 1).collect();
     fn temp_func(ll: Vec<u32>) -> u32 {
         if ll.len() != 1 {
             let mut temp = filter_sequence(ll);
             temp.reverse();
+            //println!("{:?}", temp);
             temp_func(temp)
         } else {
             *ll.first().unwrap()
@@ -24,4 +25,5 @@ fn last_remaining(n: u32) -> u32 {
 fn main() {
     //println!("{:?}", filter_sequence(vec![1, 2, 3, 4, 5, 6, 7]));
     println!("{:?}", last_remaining(10));
+    println!("{:?}", last_remaining(9));
 }
