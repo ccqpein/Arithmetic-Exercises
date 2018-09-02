09c5c5d5a6caa0a74a94c16e0f7cf33abfb2199e
ccQpein <ccqpein@protonmail.com>
2018-09-02 12:52:45 -0400

    a bit

--- Dijkstra-algorithm/DA.rs
+++ Dijkstra-algorithm/DA.rs
@@ -1,19 +1,22 @@
-#![feature(macro_rules)]
-use std::any::{Any, TypeId};
+//#![feature(macro_rules)]
+//use std::any::{Any, TypeId};
+use std::cmp::Ordering;
+use std::collections::HashMap;
 use std::ops::{Add, Sub};
+
+/*
+{start: }
+*/
+type Matrix<T> = HashMap<&'static str, HashMap<&'static str, Option<T>>>;
 
 struct DistanceMatrix<T> {
     length: usize,
-    inner_max: Vec<Vec<Option<T>>>,
+    inner_max: Matrix<T>,
 }
-
-type Matrix<T> = Vec<Vec<Option<T>>>;
-
-struct nil;
 
 impl<T> DistanceMatrix<T>
 where
-    T: Add + Sub,
+    T: Add + Sub + Ord,
 {
     fn new(input: Matrix<T>) -> Self {
         DistanceMatrix {
@@ -21,13 +24,19 @@ where
             inner_max: input,
         }
     }
+
+    fn new_dijkstra_on(&mut self, end: &'static str) {}
 }
 
+//struct nil;
+// type check function template
+/* 
 fn is_nil<T: ?Sized + Any>(o: &T) -> bool {
     TypeId::of::<i32>() == TypeId::of::<T>()
-}
+}*/
 
-// new_distance_matrix!(3,[[1,2,nil],[2,3,nil],[4,nil,6]])
+// new_distance_matrix!()
+// cannot check type in macro body. so do not need this macro
 macro_rules! new_distance_matrix {
     ($([$($x:expr),*]),*) => {{
         vec![$(vec![$(
@@ -44,8 +53,10 @@ macro_rules! new_distance_matrix {
 }
 
 fn main() {
-    let test0 = new_distance_matrix![[&1, &2, &3], [&4, &5, &6], [&7, &8, &_]];
+    let test0: Matrix<u32> = [(
+        "a",
+        [("b", Some(6)), ("c", Some(3))] as HashMap<&'static str, Option<u32>>,
+    )];
     println!("{:?}", test0);
     //println!("{:?}", new_distance_matrix![[1, 2, 3], [4, 5, 6], [7, 8]]);
-    println!("{:?}", is_nil(&nil {}));
 }
