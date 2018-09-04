a44e75618a08e7103816e2d0f27be27fbb965f36
ccQpein <ccqpein@protonmail.com>
2018-09-04 09:44:09 -0400

    finish, but I need write a macro

--- Dijkstra-algorithm/DA.rs
+++ Dijkstra-algorithm/DA.rs
@@ -56,6 +56,7 @@ where
     }
 
     fn new_dijkstra_on(&mut self, start: &'static str) {
+        // all nodes set
         let mut all_notes = self
             .inner_max
             .keys()
@@ -63,41 +64,40 @@ where
             .collect::<HashSet<&'static str>>();
         all_notes.remove(start);
 
-        let mut smallest_nodes_connected: (&'static str, Detail<T>);
+        let mut smallest_nodes_connected: &'static str;
         {
             let this_nodes_data = match self.inner_max.get(start) {
-                Some(data) => data.clone(),
+                Some(data) => data.clone(), // need clone here
                 None => {
                     println!("{}", "do not have this node");
                     return;
                 }
             };
 
-            // find shortest node of start
-            let (a, b) = this_nodes_data
+            // find smallest node
+            let (a, _) = this_nodes_data
                 .iter()
                 .map(|x| x)
+                .filter(|x| all_notes.contains(x.0)) // must nodes in all nodes.
                 .min_by_key(|(_, v)| v.0)
                 .unwrap();
-            smallest_nodes_connected = (*a, b.clone());
+            smallest_nodes_connected = *a;
         }
 
-        all_notes.remove(smallest_nodes_connected.0);
+        //all_notes.remove(smallest_nodes_connected);
 
-        let mut distance_to_now: T;
-        let mut path_to_now: Vec<&'static str>;
-
-        println!("{:?}", smallest_nodes_connected.0);
+        //println!("{:?}", smallest_nodes_connected);
         while (!all_notes.is_empty()) {
             let distance_to_now: T;
             let path_to_now: Vec<&'static str>;
 
+            all_notes.remove(smallest_nodes_connected);
             {
-                distance_to_now = self.val_between(start, smallest_nodes_connected.0).unwrap();
-                path_to_now = self.path(start, smallest_nodes_connected.0).unwrap();
+                distance_to_now = self.val_between(start, smallest_nodes_connected).unwrap();
+                path_to_now = self.path(start, smallest_nodes_connected).unwrap();
             }
 
-            let this_nodes_data = match { self.inner_max.get_mut(smallest_nodes_connected.0) } {
+            let this_nodes_data = match { self.inner_max.get_mut(smallest_nodes_connected) } {
                 Some(data) => data.clone(),
                 None => {
                     println!("{}", "do not have this node");
@@ -106,15 +106,26 @@ where
             };
 
             //update start node
+            let mut this_round_result: Vec<(&'static str, T)> = vec![];
             for (k, v) in this_nodes_data.iter() {
+                // cannot be start node
+                if *k == start {
+                    continue;
+                }
+
                 if let Some(v_old) = self.val_between(start, k) {
-                    if (distance_to_now + v.0) < { v_old } {
+                    if (distance_to_now + v.0) < v_old {
                         let mut new_path = path_to_now.clone();
                         new_path.push(k);
                         self.inner_max
                             .get_mut(start)
                             .unwrap()
                             .insert(k, (distance_to_now + v.0, new_path));
+                        this_round_result.push((*k, distance_to_now + v.0));
+                    } else {
+                        // if old value is smaller one, dont change path
+                        // but push result in this_round_result
+                        this_round_result.push((*k, v_old));
                     }
                 } else {
                     let mut new_path = path_to_now.clone();
@@ -123,10 +134,22 @@ where
                         .get_mut(start)
                         .unwrap()
                         .insert(k, (distance_to_now + v.0, new_path));
+                    this_round_result.push((*k, distance_to_now + v.0));
                 }
             }
 
-            all_notes.remove(smallest_nodes_connected.0);
+            // next smallest node
+            smallest_nodes_connected = if let Some(a) = this_round_result
+                .iter()
+                .filter(|x| all_notes.contains(x.0))
+                .min_by_key(|(_, v)| v)
+            {
+                a.0
+            } else {
+                ""
+            };
+
+            //println!("{:?}", smallest_nodes_connected);
         }
     }
 }
@@ -161,33 +184,83 @@ fn main() {
     //println!("{:?}", test0);
     //println!("{:?}", new_distance_matrix![[1, 2, 3], [4, 5, 6], [7, 8]]);
 
-    let mut testcaseM1: Matrix<u32> = HashMap::new();
-    testcaseM1.insert(
+    let mut test_caseM1: Matrix<u32> = HashMap::new();
+    test_caseM1.insert(
         "a",
-        [("b", (6 as u32, vec![])), ("c", (3 as u32, vec![]))]
-            .iter()
-            .cloned()
-            .collect(),
-    );
-    testcaseM1.insert(
-        "b",
         [
-            ("a", (6 as u32, vec![])),
-            ("c", (2 as u32, vec![])),
-            ("d", (5 as u32, vec![])),
+            ("b", (6 as u32, vec!["a", "b"])),
+            ("c", (3 as u32, vec!["a", "c"])),
         ]
             .iter()
             .cloned()
             .collect(),
     );
-    println!("{:?}", testcaseM1);
+    test_caseM1.insert(
+        "b",
+        [
+            ("a", (6 as u32, vec!["b", "a"])),
+            ("c", (2 as u32, vec!["b", "c"])),
+            ("d", (5 as u32, vec!["b", "d"])),
+        ]
+            .iter()
+            .cloned()
+            .collect(),
+    );
+    test_caseM1.insert(
+        "c",
+        [
+            ("a", (3 as u32, vec!["c", "a"])),
+            ("d", (3 as u32, vec!["c", "d"])),
+            ("b", (2 as u32, vec!["c", "b"])),
+            ("e", (4 as u32, vec!["c", "e"])),
+        ]
+            .iter()
+            .cloned()
+            .collect(),
+    );
+    test_caseM1.insert(
+        "d",
+        [
+            ("b", (5 as u32, vec!["d", "b"])),
+            ("e", (2 as u32, vec!["d", "e"])),
+            ("c", (3 as u32, vec!["d", "c"])),
+            ("f", (3 as u32, vec!["d", "f"])),
+        ]
+            .iter()
+            .cloned()
+            .collect(),
+    );
+    test_caseM1.insert(
+        "e",
+        [
+            ("c", (4 as u32, vec!["e", "c"])),
+            ("d", (2 as u32, vec!["e", "d"])),
+            ("f", (5 as u32, vec!["e", "f"])),
+        ]
+            .iter()
+            .cloned()
+            .collect(),
+    );
+    test_caseM1.insert(
+        "f",
+        [
+            ("e", (5 as u32, vec!["f", "e"])),
+            ("d", (3 as u32, vec!["f", "e"])),
+        ]
+            .iter()
+            .cloned()
+            .collect(),
+    );
 
-    let mut testcase1: DistanceMatrix<u32>;
-    testcase1 = DistanceMatrix {
+    println!("{:?}", test_caseM1);
+
+    let mut test_case1: DistanceMatrix<u32>;
+    test_case1 = DistanceMatrix {
         length: 4,
-        inner_max: testcaseM1,
+        inner_max: test_caseM1,
     };
 
     //Option<(&&str, &(u32, std::vec::Vec<u32>))>
-    println!("{:?}", testcase1.new_dijkstra_on("a"));
+    println!("{:?}", test_case1.new_dijkstra_on("a"));
+    println!("{:?}", test_case1.inner_max.get("a"));
 }
