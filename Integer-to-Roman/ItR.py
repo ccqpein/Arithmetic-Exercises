f3d740e412213a894363c5595df6edd41f7ebde6
ccQpein <ccqpein@protonmail.com>
2018-09-20 18:25:34 -0400

    finish good version

--- Integer-to-Roman/ItR.py
+++ Integer-to-Roman/ItR.py
@@ -97,3 +97,47 @@ class Solution:
                 break
 
         return result
+
+    def intToRoman2(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        table = {
+            1: "I",
+            4: "IV",
+            5: "V",
+            9: "IX",
+            10: "X",
+            40: "XL",
+            50: "L",
+            90: "XC",
+            100: "C",
+            400: "CD",
+            500: "D",
+            900: "CM",
+            1000: "M",
+        }
+
+        number = num
+        result = ""
+        order = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+
+        while True:
+            if number == 0:
+                break
+            if number in table:
+                result += table[number]
+                break
+
+            for v in order:
+                if number > v:
+                    for i in range(number//v):
+                        result += table[v]
+                    number %= v
+                    break
+        return result
+
+
+a = Solution()
+a.intToRoman2(1)
