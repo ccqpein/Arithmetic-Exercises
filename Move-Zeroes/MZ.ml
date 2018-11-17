8eb4e4abfc3e89f747afa17212b21c6123ed6091
ccQpein <ccqpein@protonmail.com>
2018-11-17 08:41:12 -0500

    finish ocaml version

--- Move-Zeroes/MZ.ml
+++ Move-Zeroes/MZ.ml
@@ -1,5 +1,13 @@
 let mz =
-  List.fold_right (fun x acc -> if x != 0
-                                then x::acc
-                                else List.append acc [x])
+  List.fold_right
+    (fun x acc -> if x != 0
+                  then x::acc
+                  else List.append acc [x])
 ;;
+
+let rec print_list = function
+                   |[] -> ()
+                   | e::l -> print_int e ; print_string " " ; print_list l
+;;
+
+print_list (mz [0;1;0;12;11] []);;
