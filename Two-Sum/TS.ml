d35ee5e508b33a1ff340a88926e5a754f941b543
ccQpein <ccqpein@protonmail.com>
2018-11-19 00:45:20 -0500

    finish ocaml version

--- Two-Sum/TS.ml
+++ Two-Sum/TS.ml
@@ -1,2 +1,21 @@
-let two_sum nums target:int :(int*int) =
-  let result = ref 
+exception Empty_list;;
+exception Nothing;;
+
+let rec num_sum nums target ind=
+  let rec inner_func num rest target ind=
+    match rest with
+    |[] -> raise Nothing;
+    | x::xs -> if num +  x  = target
+               then ind
+               else inner_func num xs target (ind + 1)
+  in
+  match nums with
+  |[] -> raise Empty_list
+  |x::xs -> try (ind, inner_func x xs target (ind+1))
+            with Nothing ->
+              num_sum xs target (ind+1)
+;;
+
+num_sum [2;7;11;15] 9 0;;
+num_sum [0;4;3;0] 0 0;;
+num_sum [-3;4;3;90] 0 0;;
