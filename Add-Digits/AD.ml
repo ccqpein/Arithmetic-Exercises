9f212e3cbbb9c0886c95072710bc7b6d94cfe67b
ccQpein <ccqpein@protonmail.com>
2018-12-12 22:15:55 -0500

    a bit

--- Add-Digits/AD.ml
+++ Add-Digits/AD.ml
@@ -3,7 +3,7 @@ let rec add_digits (num: int) : int =
                    (List.init
                       (String.length (string_of_int num))
                       (String.get (string_of_int num)))
-  and sum l :int = List.fold_left (+) 0 l;
+  and sum :'int list -> int = List.fold_left (+) 0;
   in
   if num <0 then -1
   else if num >= 10 then add_digits (sum (sepa num))
