5e1a9002fe1d92263677c8b831604f4aeacb9962
ccQpein <ccqpein@protonmail.com>
2019-05-23 18:17:48 -0400

    finish lisp version

--- Remove-All-Adjacent-Duplicates-In-String/RAADIS.lisp
+++ Remove-All-Adjacent-Duplicates-In-String/RAADIS.lisp
@@ -1,1 +1,13 @@
-(defun remove-duplicates)
+(defun remove-duplicates- (str)
+  (let ((stack '()))
+    (do* ((strs (coerce str 'list) (cdr strs))
+          (this (car strs) (car strs))
+          (last)
+          )
+         ((eq strs nil) (concatenate 'string stack))
+
+      (if (eq this last)
+          (setf stack (butlast stack)
+                last (car (last stack)))
+          (progn (setf last this
+                       stack (append stack (list this))))))))
