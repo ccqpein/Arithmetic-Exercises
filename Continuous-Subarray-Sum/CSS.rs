a78c4fadbf4f43b8a86339641c0825c3d6360abc
ccQpein <ccqpein@protonmail.com>
2019-07-24 09:49:14 -0400

    finish rust version

--- Continuous-Subarray-Sum/CSS.rs
+++ Continuous-Subarray-Sum/CSS.rs
@@ -6,26 +6,36 @@ fn sum_vec(nums: &Vec<i32>) -> i32 {
     result
 }
 
-fn check_sub_array_sum(nums: Vec<i32>, k: i32) -> bool {
-    let length = nums.len();
-    if length <= 1 {
+pub fn check_subarray_sum(nums: Vec<i32>, k: i32) -> bool {
+    if nums.len() == 1 {
         return false;
     }
-
-    if sum_vec(&nums) == 0 {
-        return true;
+    let mut slice_copy = nums.as_slice();
+    for count in 1..nums.len() {
+        for start in 0..nums.len() - count {
+            match k {
+                0 => {
+                    if slice_copy[start..start + count + 1].iter().sum::<i32>() == 0 {
+                        return true;
+                    }
+                }
+                _ => {
+                    if slice_copy[start..start + count + 1].iter().sum::<i32>() % k.abs() == 0 {
+                        return true;
+                    }
+                }
+            }
+        }
     }
 
-    if k == 0 {
-        return false;
-    }
-
-    if length >= 2 && k > 0 {
-        return true;
-    }
-
-    let inner_k = k.abs();
-    //not finish
+    false
 }
 
-fn main() {}
+fn main() {
+    dbg!(check_subarray_sum(vec![23, 2, 4, 6, 7], 6));
+    dbg!(check_subarray_sum(vec![23, 2, 6, 4, 7], -6));
+    dbg!(check_subarray_sum(vec![0, 0], -1));
+    dbg!(check_subarray_sum(vec![1, 2, 3], 4));
+    dbg!(check_subarray_sum(vec![0, 0], 0));
+    dbg!(check_subarray_sum(vec![0], 0));
+}
