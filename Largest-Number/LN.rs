a965594c794460a3306974dfeaeec85517d1d023
ccQpein <ccqpein@protonmail.com>
2019-07-31 22:27:18 -0400

    still a bit

--- Largest-Number/LN.rs
+++ Largest-Number/LN.rs
@@ -1,3 +1,7 @@
-pub fn largest_number(nums: Vec<i32>) -> String {}
+pub fn largest_number(nums: Vec<i32>) -> String {
+    for n in nums {
+        let this = n.to_string();
+    }
+}
 
 fn main() {}
