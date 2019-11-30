bad37565a18bf964e905f8bcdd3b7c98365df3b5
ccQpein <ccqpein@protonmail.com>
2019-11-30 00:14:07 -0500

    finish new

---
+++ Distribute-Coins-in-Binary-Tree/DCIBT.rs
@@ -0,0 +1,39 @@
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub fn distribute_coins(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+    let mut total = 0;
+    dfs(root.as_ref(), &mut total);
+    total
+}
+
+fn dfs(root: Option<&Rc<RefCell<TreeNode>>>, total: &mut i32) -> i32 {
+    if root.is_none() {
+        return 0;
+    }
+
+    let (l, r) = (
+        dfs(root.unwrap().borrow().left.as_ref(), total),
+        dfs(root.unwrap().borrow().right.as_ref(), total),
+    );
+    *total += l.abs() + r.abs();
+    root.unwrap().borrow().val + l + r - 1
+}
