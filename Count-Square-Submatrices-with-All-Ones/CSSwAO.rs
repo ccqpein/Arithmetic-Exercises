7af1af48a574ac6a015fe1e38c817bd138f2c7bd
ccQpein <ccqpein@protonmail.com>
2020-02-08 13:21:38 -0500

    finish new

--- Count-Square-Submatrices-with-All-Ones/CSSwAO.rs
+++ Count-Square-Submatrices-with-All-Ones/CSSwAO.rs
@@ -2,12 +2,48 @@ pub fn count_squares(matrix: Vec<Vec<i32>>) -> i32 {
     let row = matrix.len();
     let col = matrix[0].len();
 
-    let mut all_matrix: Vec<i32> = vec![];
-    for si in 1..=col {
-        if si > row {
-            break;
+    let whole_size = if row <= col { row } else { col };
+
+    let mut count = 0;
+    for r in 0..row {
+        for c in 0..col {
+            for si in 1..=whole_size {
+                if r + si > row || c + si > col {
+                    break;
+                }
+
+                if pick_matrix(&matrix, r, c, si) {
+                    count += 1;
+                } else {
+                    continue;
+                }
+            }
         }
     }
+    count
 }
 
-fn main() {}
+fn pick_matrix(matrix: &Vec<Vec<i32>>, row: usize, col: usize, size: usize) -> bool {
+    for r in row..row + size {
+        if !matrix[r][col..col + size].iter().all(|x| *x == 1) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+fn main() {
+    dbg!(pick_matrix(
+        &vec![vec![0, 1, 1, 1], vec![1, 1, 1, 1], vec![0, 1, 1, 1]],
+        0,
+        1,
+        2
+    ));
+
+    dbg!(count_squares(vec![
+        vec![0, 1, 1, 1],
+        vec![1, 1, 1, 1],
+        vec![0, 1, 1, 1]
+    ]));
+}
