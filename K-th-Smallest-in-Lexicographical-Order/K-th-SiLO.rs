e7b6d582fbf1fc3497f0fd219516f4cc246216b9
ccQpein <ccqpein@protonmail.com>
2020-03-26 10:57:20 -0400

    optimise failed, copy other guys solution
    https://leetcode.com/problems/k-th-smallest-in-lexicographical-order/discuss/92242/ConciseEasy-to-understand-Java-5ms-solution-with-Explaination

--- K-th-Smallest-in-Lexicographical-Order/K-th-SiLO.rs
+++ K-th-Smallest-in-Lexicographical-Order/K-th-SiLO.rs
@@ -1,40 +1,19 @@
-// pub fn find_kth_number(n: i32, k: i32) -> i32 {
-//     let mut count = k;
-//     for l in 1.. {
-//         count -= 1;
-//         if count == 0 {
-//             return l;
-//         }
-
-//         let result = inner_rec(l, n, &mut count);
-//         if result != 0 {
-//             return result;
-//         };
-//     }
-//     0
-// }
-
-// fn inner_rec(head: i32, k: i32, count: &mut i32) -> i32 {
-//     if head * 10 < n {
-//         0
-//     }
-//     0
-// }
-
-pub fn find_kth_number(n: i32, k: i32) -> i32 {
-    let mut count = k;
-    return test_inner_rec(1 as u64, &(n as u64), &mut count);
+pub fn find_kth_number_old(n: i32, k: i32) -> i32 {
+    let mut count = k as u64;
+    return test_inner_rec(1 as u64, &(n as u64), &mut count) as i32;
 }
 
-fn test_inner_rec(head: u64, n: &u64, count: &mut i32) -> i32 {
+// this version not fast enough, but it works.
+fn test_inner_rec(head: u64, n: &u64, count: &mut u64) -> u64 {
     if head > *n {
         return 0;
     }
+
     //print!("{:?} ", head);
     *count -= 1;
 
     if *count == 0 {
-        return head as i32;
+        return head;
     }
 
     let re = test_inner_rec(head * 10, n, count);
@@ -48,10 +27,44 @@ fn test_inner_rec(head: u64, n: &u64, count: &mut i32) -> i32 {
     }
 }
 
+///////////////////////////////////////
+
+pub fn find_kth_number(n: i32, k: i32) -> i32 {
+    let mut curr = 1;
+
+    let mut kk = (k - 1) as u64;
+    while kk > 0 {
+        let steps = cal_steps(n, curr, curr + 1);
+        //println!("{}", steps);
+        if steps <= kk {
+            curr += 1;
+            kk -= steps;
+        } else {
+            curr *= 10;
+            kk -= 1;
+        }
+    }
+
+    return curr as i32;
+}
+
+fn cal_steps(n: i32, n1: u64, n2: u64) -> u64 {
+    use std::cmp;
+
+    let mut steps = 0;
+    let nn = n as u64;
+    let (mut n11, mut n22) = (n1, n2);
+    while n11 <= nn {
+        steps += cmp::min(nn + 1, n22) - n11;
+        n11 *= 10;
+        n22 *= 10;
+    }
+    return steps;
+}
+
 fn main() {
     let testcase0 = 13;
     let k = 2;
-    //dbg!(test_inner_rec(1, &testcase0, &mut k));
     //dbg!(find_kth_number(testcase0, k)); // => 10
 
     let testcase1 = 100;
@@ -60,9 +73,13 @@ fn main() {
 
     let testcase1 = 681692778;
     let k = 351251360;
-    //dbg!(find_kth_number(testcase1, k)); // => 416126219
+    dbg!(find_kth_number(testcase1, k)); // => 416126219
 
     let testcase1 = 957747794;
     let k = 424238336;
-    dbg!(find_kth_number(testcase1, k)); // => 416126219
+    dbg!(find_kth_number(testcase1, k)); // => 481814499
+
+    let testcase2 = 10000;
+    let mut k = 10000;
+    dbg!(find_kth_number(testcase2, k)); // => 9999
 }
