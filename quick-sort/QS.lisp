12d8beb85e56e60a7cecde221526547e0355c116
ccQpein <ccqpein@protonmail.com>
2020-05-12 12:00:18 -0400

    add new method

--- quick-sort/QS.lisp
+++ quick-sort/QS.lisp
@@ -9,3 +9,22 @@
                 (quick-sort
                  (loop for i in (cdr l) when (>= i key)
                     collect i))))))
+
+
+(defun quick-sort-2 (l start end)
+  (if (>= start end) (return-from quick-sort-2))
+  (let ((copy-start start)
+        (copy-end end))
+    
+    (loop while (/= start end)
+          do (if (> (nth start l) (nth end l))
+                 (let ((temp (nth end l)))
+                   (setf (nth end l) (nth start l)
+                         end (1- end)
+                         (nth start l) (nth end l)
+                         (nth end l) temp))
+                 (setf start (1+ start))))
+
+    (quick-sort-2 l copy-start (1- start))
+    (quick-sort-2 l (1+ end) copy-end)
+    ))
