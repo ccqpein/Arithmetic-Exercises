8a6babd4694cca346da9ba6a25163831d7b05066
ccQpein <ccqpein@protonmail.com>
2020-05-17 12:42:40 -0400

    finish first one

--- Happy-Number/HN.jl
+++ Happy-Number/HN.jl
@@ -1,9 +1,26 @@
 function isHappy(s)::Bool
-    a = s isa Number ? (try
-                        parse(Int64, s)
-                        catch e
-                        println("Wrong type $e")
-                        end) : s
-    println(a)
-    true
+    a::Int = s isa Number ? s : (try
+                                 parse(Int64, s)
+                                 catch e
+                                 throw(error("Wrong type $e"))
+                                 end)
+    innerHappy(a, Set([]))
+end
+
+function eachBit(n::Int)
+    result = []
+    while n != 0
+        let left = n % 10
+            push!(result, left)
+            n = Int((n-left)/10)
+        end
+    end
+    result
+end
+
+function innerHappy(n,se)
+    n in se && return false
+    next = sum(eachBit(n) .^ 2)
+    next == 1 ? (return true) : (se = union(se, n))
+    innerHappy(next,se)
 end
