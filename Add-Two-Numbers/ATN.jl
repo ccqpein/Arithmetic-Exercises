24b11c86990e4222a4d280a7eca5e72e4d307514
ccq <ccqpein@protonmail.com>
2020-06-06 17:32:13 -0400

    fixed

--- Add-Two-Numbers/ATN.jl
+++ Add-Two-Numbers/ATN.jl
@@ -1,9 +1,13 @@
 mutable struct ListNode{T<:Number}
     val :: T
-    next:: ListNode
-    ListNode{T}(data::Array{T}) where T<:Number = begin
+    next:: Union{ListNode, Nothing}    
+
+    ListNode(data::Array{T,1}) where T<:Number = begin
         (head, rest) = Iterators.peel(data)
-        new(head, new(collect(rest)))
+        if isempty(rest)
+            new{T}(head::T, nothing)
+        else
+            new{T}(head, ListNode{T}(collect(rest) :: Array{T,1}))
+        end
     end
 end
-
