157ca2fbee4293022a1039f8d98e1fc284f3202f
ccQpein <ccqpein@protonmail.com>
2020-09-04 20:41:45 -0400

    start new

---
+++ Longest-Palindromic-Substring/LPS.rs
@@ -0,0 +1,8 @@
+//pub fn longest_palindrome(s: String) -> String {}
+
+/// find palindrome, return length, start index
+fn find_palindrome(s: &String, start: usize) -> (usize, usize) {
+    (0, 0, 0)
+}
+
+fn main() {}
