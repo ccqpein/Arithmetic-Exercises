8cd200a9d6c44cb6208bf40f31e364b846521f7f
ccQpein <ccqpein@protonmail.com>
2021-03-30 00:19:09 -0400

    new method

--- Power-of-Two/P2T.rs
+++ Power-of-Two/P2T.rs
@@ -12,6 +12,10 @@ pub fn is_power_of_two(n: i32) -> bool {
     return false;
 }
 
+pub fn is_power_of_two2(n: i32) -> bool {
+    n != 0 && (n as i64).count_ones() == 1
+}
+
 fn main() {
     assert!(is_power_of_two(16));
     assert!(is_power_of_two(1));
@@ -19,5 +23,13 @@ fn main() {
     assert!(is_power_of_two(4));
     assert!(!is_power_of_two(5));
     assert!(!is_power_of_two(0));
-    assert!(!is_power_of_two(-2147483648))
+    assert!(!is_power_of_two(-2147483648));
+
+    assert!(is_power_of_two2(16));
+    assert!(is_power_of_two2(1));
+    assert!(!is_power_of_two2(3));
+    assert!(is_power_of_two2(4));
+    assert!(!is_power_of_two2(5));
+    assert!(!is_power_of_two2(0));
+    assert!(!is_power_of_two2(-2147483648))
 }
