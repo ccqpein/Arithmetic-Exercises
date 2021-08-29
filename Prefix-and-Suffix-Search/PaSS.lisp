ff8833d6b8f265d1db7304fa7364c3e7e5a7c7ce
ccQpein <ccqpein@protonmail.com>
2021-08-29 12:45:03 -0400

    it has been a while from my last time code common lisp

--- Prefix-and-Suffix-Search/PaSS.lisp
+++ Prefix-and-Suffix-Search/PaSS.lisp
@@ -1,1 +1,19 @@
+(defstruct word-filter
+  (pre-table (make-hash-table :test 'equal) :type hash-table)
+  (pro-table (make-hash-table :test 'equal) :type hash-table))
+
+(defun f (words)
+  ;;(declare (list string words))
+  (let ((wf (make-word-filter))
+        (w-words (loop
+                   with index = 0
+                   and table = (make-hash-table :test 'equal)
+                   for word of-type string in words
+                   do (setf (gethash word table) index)
+                   do (incf index)
+                   finally (return table))))
+    w-words))
+
 (defun main ())
+
+
