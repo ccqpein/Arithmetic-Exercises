63bc8cfacf1e5b6f5123a88673b111482058b187
ccQpein <ccqpein@protonmail.com>
2021-08-30 18:23:08 -0400

    lisp version

--- Prefix-and-Suffix-Search/PaSS.lisp
+++ Prefix-and-Suffix-Search/PaSS.lisp
@@ -1,8 +1,8 @@
-(defstruct word-filter
+(defstruct (word-filter (:conc-name wf-))
   (pre-table (make-hash-table :test 'equal) :type hash-table)
   (pro-table (make-hash-table :test 'equal) :type hash-table))
 
-(defun f (words)
+(defun new-word-filter (words)
   ;;(declare (list string words))
   (let ((wf (make-word-filter))
         (w-words (loop
@@ -12,8 +12,43 @@
                    do (setf (gethash word table) index)
                    do (incf index)
                    finally (return table))))
-    w-words))
+    (loop
+      for word being each hash-key of w-words
+        using (hash-value ind)
+      do (loop
+           for index from 1 to (length word)
+           for prefix = (subseq word 0 index)
+           and suffix = (subseq word (- (length word) index) (length word))
 
-(defun main ())
+           when (not (gethash prefix (wf-pre-table wf)))
+             ;; fake hash-set, key is fixnum
+             do (setf (gethash prefix (wf-pre-table wf)) (make-hash-table)) 
+           end
 
+           when (not (gethash suffix (wf-pro-table wf)))
+             ;; fake hash-set, key is fixnum
+             do (setf (gethash suffix (wf-pro-table wf)) (make-hash-table)) 
+           end
 
+           do (setf (gethash ind (gethash prefix (wf-pre-table wf))) 0
+                    (gethash ind (gethash suffix (wf-pro-table wf))) 0)
+           ))
+    wf))
+
+(defun return-all-keys (table)
+  (loop for k being each hash-key of table
+        collect k))
+
+(defun f (wf preffix suffix)
+  (let ((k1 (return-all-keys (gethash preffix (wf-pre-table wf))))
+        (k2 (return-all-keys (gethash suffix (wf-pro-table wf)))))
+    (let ((result (intersection k1 k2)))
+      (if (not result)
+          -1
+          (apply #'max result)))
+    ))
+
+(defun main ()
+  (let ((wf (new-word-filter '("cabaabaaaa" "ccbcababac" "bacaabccba" "bcbbcbacaa" "abcaccbcaa" "accabaccaa" "cabcbbbcca" "ababccabcb" "caccbbcbab" "bccbacbcba"))))
+    (assert (= 5 (f wf "a" "aa")))
+    (assert (= 9 (f wf "bccbacbcba" "a")))))
