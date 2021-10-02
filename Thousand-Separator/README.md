d24c25a76a4988e3629d6024dbb67f9ee75d23de
ccQpein <ccqpein@protonmail.com>
2021-10-02 12:37:40 -0400

    new

---
+++ Thousand-Separator/README.md
@@ -0,0 +1,51 @@
+# README #
+
+[source](https://leetcode.com/problems/thousand-separator/)
+
+Given aninteger `n`, add a dot (&quot;.&quot;)as the thousands separator and return it instring format.
+
+
+**Example 1:**
+
+```
+**Input:** n = 987
+**Output:** &quot;987&quot;
+
+```
+
+
+**Example 2:**
+
+```
+**Input:** n = 1234
+**Output:** &quot;1.234&quot;
+
+```
+
+
+**Example 3:**
+
+```
+**Input:** n = 123456789
+**Output:** &quot;123.456.789&quot;
+
+```
+
+
+**Example 4:**
+
+```
+**Input:** n = 0
+**Output:** &quot;0&quot;
+
+```
+
+
+
+**Constraints:**
+
+
+	+ `0 &lt;= n &lt; 2^31`
+
+
+
---
+++ Thousand-Separator/TS.rs
@@ -0,0 +1,25 @@
+use std::iter::FromIterator;
+
+pub fn thousand_separator(n: i32) -> String {
+    let ns = n.to_string();
+    let mut result = vec![];
+    let mut count = 0;
+    for c in ns.chars().rev() {
+        if count == 3 {
+            count = 1;
+            result.push('.');
+        } else {
+            count += 1;
+        }
+        result.push(c);
+    }
+    String::from_iter(result.iter().rev())
+}
+
+fn main() {
+    dbg!(thousand_separator(997));
+    dbg!(thousand_separator(1234));
+    dbg!(thousand_separator(123456789));
+    dbg!(thousand_separator(0));
+    dbg!(thousand_separator(0));
+}
