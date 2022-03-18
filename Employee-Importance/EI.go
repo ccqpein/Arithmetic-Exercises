a419e172a3fbb35b7c40fa280231d9b8f087b2f7
ccQpein <ccqpein@protonmail.com>
2022-03-18 01:24:45 -0400

    a bit

--- Employee-Importance/EI.go
+++ Employee-Importance/EI.go
@@ -6,12 +6,12 @@ type Employee struct {
 	Subordinates []int
 }
 
-func getImportance(employees []*Employee, id int) int {
+func getImportance_bkp(employees []*Employee, id int) int {
 	for _, e := range employees {
 		if e.Id == id {
 			next := 0
 			for _, sub := range e.Subordinates {
-				next += getImportance(employees, sub)
+				next += getImportance_bkp(employees, sub)
 			}
 			return next + e.Importance
 		}
@@ -19,6 +19,25 @@ func getImportance(employees []*Employee, id int) int {
 	}
 
 	return 0
+}
+
+// even slower
+func getImportance(employees []*Employee, id int) int {
+	table := map[int]*Employee{}
+	for _, e := range employees {
+		table[e.Id] = e
+	}
+
+	var inner func(ee map[int]*Employee, id int) int
+	inner = func(ee map[int]*Employee, id int) int {
+		result := ee[id].Importance
+		for _, nextId := range ee[id].Subordinates {
+			result += inner(ee, nextId)
+		}
+		return result
+	}
+
+	return inner(table, id)
 }
 
 func main() {}
