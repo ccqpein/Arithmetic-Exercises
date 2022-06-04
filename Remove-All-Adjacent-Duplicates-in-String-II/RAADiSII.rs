b6c5c1f39429f5e976213876234b858af99577d3
ccQpein <ccqpein@protonmail.com>
2022-06-04 00:49:57 -0400

    finish

--- Remove-All-Adjacent-Duplicates-in-String-II/RAADiSII.rs
+++ Remove-All-Adjacent-Duplicates-in-String-II/RAADiSII.rs
@@ -1,24 +1,31 @@
 pub fn remove_duplicates(s: String, k: i32) -> String {
     let mut stack: Vec<char> = vec![];
     let k = k as usize;
-    let (mut last_count, mut last_count_vec, mut last) = (0, vec![], ' ');
+    let mut cc = s.chars();
+    let first = cc.next().unwrap();
+    let (mut last_count, mut last_count_vec, mut last) = (1, vec![], first);
+    stack.push(first);
 
-    for c in s.chars() {
-        println!(
-            "last_count: {}, last_count_vec: {:?}, last: {}",
-            last_count, last_count_vec, last
-        );
-
+    for c in cc {
+        // println!(
+        //     "last_count: {}, last_count_vec: {:?}, last: {}, stack: {:?}",
+        //     last_count, last_count_vec, last, stack
+        // );
         if c == last {
             last_count += 1;
+            stack.push(c);
+
             if last_count == k {
-                stack.truncate(stack.len() - k + 1);
-                last_count_vec.pop();
+                stack.truncate(stack.len() - k);
+                //println!("stack here? {:?}", stack);
+                last_count = last_count_vec.pop().unwrap_or(0);
+                last = *stack.last().unwrap_or(&' ');
             }
         } else {
-            last_count_vec.push(last_count + 1);
+            last_count_vec.push(last_count);
             last = c;
-            last_count = 0;
+            last_count = 1;
+            stack.push(c);
         }
     }
 
@@ -27,4 +34,6 @@ pub fn remove_duplicates(s: String, k: i32) -> String {
 
 fn main() {
     dbg!(remove_duplicates("deeedbbcccbdaa".into(), 3));
+    dbg!(remove_duplicates("pbbcggttciiippooaais".into(), 2));
+    dbg!(remove_duplicates("abcd".into(), 2));
 }
