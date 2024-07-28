58763ae86c8487bd07f14e2651404c3e0b984bd2
ccQpein <ccqpein@protonmail.com>
2024-07-27 23:02:25 -1000

    add new

---
+++ minimum-operations-to-make-array-equal-to-target/README.md
@@ -0,0 +1,49 @@
+# README #
+[source](https://leetcode.com/problems/minimum-operations-to-make-array-equal-to-target/)
+
+You are given two positive integer arrays `nums` and `target`, of the same length.
+
+In a single operation, you can select any <span data-keyword="subarray">subarray</span> of `nums` and increment or decrement each element within that subarray by 1.
+
+Return the **minimum** number of operations required to make `nums` equal to the array `target`.
+
+
+**Example 1:**
+
+<div class="example-block">
+**Input:** <span class="example-io">nums = [3,5,1,2], target = [4,6,2,4]</span>
+
+**Output:** <span class="example-io">2</span>
+
+**Explanation:**
+
+We will perform the following operations to make `nums` equal to `target`:**
+- Increment`nums[0..3]` by 1, `nums = [4,6,2,3]`.**
+- Increment`nums[3..3]` by 1, `nums = [4,6,2,4]`.
+</div>
+
+**Example 2:**
+
+<div class="example-block">
+**Input:** <span class="example-io">nums = [1,3,2], target = [2,1,4]</span>
+
+**Output:** <span class="example-io">5</span>
+
+**Explanation:**
+
+We will perform the following operations to make `nums` equal to `target`:**
+- Increment`nums[0..0]` by 1, `nums = [2,3,2]`.**
+- Decrement`nums[1..1]` by 1, `nums = [2,2,2]`.**
+- Decrement`nums[1..1]` by 1, `nums = [2,1,2]`.**
+- Increment`nums[2..2]` by 1, `nums = [2,1,3]`.**
+- Increment`nums[2..2]` by 1, `nums = [2,1,4]`.
+</div>
+
+
+**Constraints:**
+
+
++ `1 <= nums.length == target.length <= 10^5`
++ `1 <= nums[i], target[i] <= 10^8`
+
+
---
+++ minimum-operations-to-make-array-equal-to-target/motmaett.rs
@@ -0,0 +1,118 @@
+fn chunk_vec(detla: Vec<i32>) -> Vec<Vec<i32>> {
+    dbg!(&detla);
+    let mut cache = vec![];
+    let mut flag = if detla[0] >= 0 { true } else { false };
+    let mut result = vec![];
+    for n in detla {
+        match flag {
+            true if n >= 0 => cache.push(n),
+            true if n < 0 => {
+                result.push(cache);
+                cache = vec![];
+                flag = false;
+                cache.push(n)
+            }
+            false if n >= 0 => {
+                result.push(cache);
+                cache = vec![];
+                flag = true;
+                cache.push(n)
+            }
+            false if n < 0 => cache.push(n),
+            _ => unreachable!(),
+        }
+    }
+
+    if cache.len() > 0 {
+        result.push(cache)
+    }
+
+    result
+        .into_iter()
+        .map(|a| {
+            a.split(|x| *x == 0)
+                .into_iter()
+                .map(|s| s.to_vec())
+                .filter(|a| a.len() > 0)
+                .collect::<Vec<_>>()
+        })
+        .flatten()
+        .collect()
+}
+
+pub fn minimum_operations(nums: Vec<i32>, target: Vec<i32>) -> i64 {
+    let detla: Vec<_> = (0..nums.len())
+        .into_iter()
+        .map(|i| target[i] - nums[i])
+        .collect();
+
+    let detla = chunk_vec(detla);
+
+    let mut result = 0;
+    for mut d in detla {
+        dbg!(&d);
+        if d[0] >= 0 {
+            // d.sort();
+            // d.into_iter().fold(0, |acc, x| {
+            //     result += (x - acc) as i64;
+            //     x
+            // });
+            sum_together(d, &mut result);
+        } else {
+            // d.sort_by(|a, b| b.partial_cmp(a).unwrap());
+            // d.into_iter().fold(0, |acc, x| {
+            //     result += -(x - acc) as i64;
+            //     x
+            // });
+            sum_together(d, &mut result);
+        };
+        //dbg!(&result);
+    }
+    result
+}
+
+fn sum_together(a: Vec<i32>, result: &mut i64) {
+    let flag = if a[0] > 0 { -1 } else { 1 };
+    let mut a = vec![a];
+    while a.len() != 0 {
+        //dbg!(&a);
+        *result += a.len() as i64;
+        a.iter_mut()
+            .for_each(|x| x.iter_mut().for_each(|xx| *xx += flag));
+
+        a = a
+            .into_iter()
+            .map(|x| split_zero(x))
+            .flatten()
+            .filter(|x| x.len() != 0)
+            .collect();
+    }
+}
+
+fn split_zero(l: Vec<i32>) -> Vec<Vec<i32>> {
+    l.split(|num| *num == 0)
+        .into_iter()
+        .map(|s| s.to_vec())
+        .collect()
+}
+
+fn main() {
+    //dbg!(chunk_vec(vec![1, 2, 3, 5, -1, -2, 3, -1, -1, 3, 4]));
+    //let mut a = vec![-3, -1, -4, -5];
+    //a.sort_by(|a, b| b.partial_cmp(a).unwrap());
+    //dbg!(a);
+
+    //dbg!(minimum_operations(vec![3, 5, 1, 2], vec![4, 6, 2, 4]));
+    //dbg!(minimum_operations(vec![1, 3, 2], vec![2, 1, 4]));
+
+    //dbg!(chunk_vec(vec![2, 0, 1, 6]));
+    //dbg!(minimum_operations(vec![5, 9, 2, 2], vec![7, 9, 3, 8]));
+    //dbg!(minimum_operations(vec![1, 1, 3, 4], vec![4, 1, 3, 2]));
+    // let mut count = 0;
+    // sum_together(vec![3, 1, 5, 3, 4, 2], &mut count);
+    // dbg!(count);
+    dbg!(minimum_operations(
+        vec![9, 2, 6, 10, 4, 8, 3, 4, 2, 3],
+        vec![9, 5, 5, 1, 7, 9, 8, 7, 6, 5]
+    ));
+}
