7075608f26eedeeecec2b211d8c9c27f1c71ea5e
ccQpein <ccqpein@protonmail.com>
2024-07-29 17:10:48 -0400

    faster solution

--- minimum-operations-to-make-array-equal-to-target/motmaett.rs
+++ minimum-operations-to-make-array-equal-to-target/motmaett.rs
@@ -40,7 +40,7 @@ fn chunk_vec(detla: Vec<i32>) -> Vec<Vec<i32>> {
         .collect()
 }
 
-pub fn minimum_operations(nums: Vec<i32>, target: Vec<i32>) -> i64 {
+pub fn minimum_operations_bkp(nums: Vec<i32>, target: Vec<i32>) -> i64 {
     let detla: Vec<_> = (0..nums.len())
         .into_iter()
         .map(|i| target[i] - nums[i])
@@ -72,13 +72,18 @@ pub fn minimum_operations(nums: Vec<i32>, target: Vec<i32>) -> i64 {
 }
 
 fn sum_together(a: Vec<i32>, result: &mut i64) {
-    let flag = if a[0] > 0 { -1 } else { 1 };
+    //let flag = if a[0] > 0 { -1 } else { 1 };
     let mut a = vec![a];
     while a.len() != 0 {
-        //dbg!(&a);
-        *result += a.len() as i64;
-        a.iter_mut()
-            .for_each(|x| x.iter_mut().for_each(|xx| *xx += flag));
+        dbg!(&a);
+
+        //*result += a.len() as i64;
+        a.iter_mut().for_each(|x| {
+            let pn = pick_num(x);
+            dbg!(&pn);
+            *result += pn.abs() as i64;
+            x.iter_mut().for_each(|xx| *xx -= pn);
+        });
 
         a = a
             .into_iter()
@@ -96,6 +101,33 @@ fn split_zero(l: Vec<i32>) -> Vec<Vec<i32>> {
         .collect()
 }
 
+fn pick_num(l: &Vec<i32>) -> i32 {
+    if l[0] > 0 {
+        *l.iter().min().unwrap()
+    } else {
+        *l.iter().max().unwrap()
+    }
+}
+
+///////////////////// faster solution below
+
+pub fn minimum_operations(nums: Vec<i32>, target: Vec<i32>) -> i64 {
+    let mut prev = 0i32;
+    let mut res = 0;
+    for (&i, &j) in nums.iter().zip(target.iter()) {
+        let diff = j - i;
+        if (diff > 0) != (prev > 0) {
+            prev = 0;
+        }
+
+        println!("{i}, {j}, {diff}, {prev}");
+
+        res += (diff.abs() - prev.abs()).max(0) as i64;
+        prev = diff;
+    }
+    res
+}
+
 fn main() {
     //dbg!(chunk_vec(vec![1, 2, 3, 5, -1, -2, 3, -1, -1, 3, 4]));
     //let mut a = vec![-3, -1, -4, -5];
@@ -111,6 +143,8 @@ fn main() {
     // let mut count = 0;
     // sum_together(vec![3, 1, 5, 3, 4, 2], &mut count);
     // dbg!(count);
+
+    //dbg!(pick_num(&vec![-1, -9]));
     dbg!(minimum_operations(
         vec![9, 2, 6, 10, 4, 8, 3, 4, 2, 3],
         vec![9, 5, 5, 1, 7, 9, 8, 7, 6, 5]
