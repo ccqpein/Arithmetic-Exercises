aac4ebf4250f840a457d2e06224e2d3f6f5878a0
ccQpein <ccqpein@protonmail.com>
2024-09-02 11:55:17 -0400

    just some clean

--- Next-Greater-Element/nge.rs
+++ Next-Greater-Element/nge.rs
@@ -4,25 +4,20 @@ pub fn next_greater_elements(nums: Vec<i32>) -> Vec<i32> {
     }
 
     let mut bucket: Vec<i32> = nums.iter().rev().map(|n| *n).collect();
-    //dbg!(&bucket);
     let mut res = Vec::with_capacity(nums.len());
     for n in nums.iter().rev() {
-        //dbg!(&res);
         loop {
             match bucket.pop() {
                 Some(nn) => {
-                    //dbg!(&nn);
-                    if nn <= *n {
-                        //bucket.pop();
-                    } else {
+                    if nn > *n {
                         bucket.push(nn);
                         bucket.push(*n);
-                        res.push(Some(nn));
+                        res.push(nn);
                         break;
                     }
                 }
                 None => {
-                    res.push(None);
+                    res.push(-1);
                     bucket.push(*n);
                     break;
                 }
@@ -30,39 +25,8 @@ pub fn next_greater_elements(nums: Vec<i32>) -> Vec<i32> {
         }
     }
 
-    // bucket.clear();
     res.reverse();
-    // for (ind, n) in nums.iter().enumerate() {
-    //     loop {
-    //         match bucket.pop() {
-    //             Some(nn) => {
-    //                 if nn <= *n {
-    //                     bucket.pop();
-    //                 } else {
-    //                     if res[ind].is_some() {
-    //                         *res[ind].as_mut().unwrap() = i32::max(nn, res[ind].unwrap());
-    //                     } else {
-    //                         res[ind] = Some(nn)
-    //                     }
-    //                     bucket.push(nn);
-    //                     bucket.push(*n);
-    //                     break;
-    //                 }
-    //             }
-    //             None => {
-    //                 bucket.push(*n);
-    //                 break;
-    //             }
-    //         }
-    //     }
-    // }
-
-    res.into_iter()
-        .map(|a| match a {
-            Some(aa) => aa,
-            None => -1,
-        })
-        .collect()
+    res
 }
 
 fn main() {
