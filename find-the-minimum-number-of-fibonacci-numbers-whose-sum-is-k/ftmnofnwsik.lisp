1b0319d22bb50426175366f7e23bc5aa39945b96
ccQpein <ccqpein@protonmail.com>
2024-11-13 15:21:57 -0500

    lisp version

---
+++ find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/ftmnofnwsik.lisp
@@ -0,0 +1,16 @@
+(defparameter *FIB-TABLE* '(1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 10946 17711 28657 46368 75025 121393 196418 317811 514229 832040 1346269 2178309 3524578 5702887 9227465 14930352 24157817 39088169 63245986 102334155 165580141 267914296 433494437 701408733 1134903170))
+
+(defun find-min-fibonacci-numbers (k)
+  (loop for ff in (reverse *FIB-TABLE*)
+        when (<= ff k)
+          sum (floor (/ k ff))
+          do (setf k (- k (* ff (floor (/ k ff)))))))
+
+(defun main ()
+  (assert (= (find-min-fibonacci-numbers 7) 2))
+  (assert (= (find-min-fibonacci-numbers 2) 1))
+  (assert (= (find-min-fibonacci-numbers 10) 2))
+  (assert (= (find-min-fibonacci-numbers 19) 3))
+  (assert (= (find-min-fibonacci-numbers 3) 1))
+  (assert (= (find-min-fibonacci-numbers 9083494) 10))
+  (assert (= (find-min-fibonacci-numbers 645157245) 13)))
