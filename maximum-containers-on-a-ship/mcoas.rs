168b5acade339ec23b4a23e93cded4c4dd45b29b
ccQpein <ccqpein@protonmail.com>
2025-03-24 20:47:58 -0400

    find the faster version

--- maximum-containers-on-a-ship/mcoas.rs
+++ maximum-containers-on-a-ship/mcoas.rs
@@ -1,14 +1,18 @@
+// pub fn max_containers(n: i32, w: i32, max_weight: i32) -> i32 {
+//     let mut re = 0;
+//     let mut count = 0;
+//     for nn in 1..=(n * n) as usize {
+//         re += w;
+//         count += 1;
+//         if re > max_weight {
+//             return count - 1;
+//         }
+//     }
+//     count
+// }
+
 pub fn max_containers(n: i32, w: i32, max_weight: i32) -> i32 {
-    let mut re = 0;
-    let mut count = 0;
-    for nn in 1..=(n * n) as usize {
-        re += w;
-        count += 1;
-        if re > max_weight {
-            return count - 1;
-        }
-    }
-    count
+    (max_weight / w).min(n * n)
 }
 
 fn main() {}
