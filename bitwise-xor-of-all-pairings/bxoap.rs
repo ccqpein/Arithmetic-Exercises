cd3a4ab0448f9a6fa7041cd92583e57e2ac69cae
ccQpein <ccqpein@protonmail.com>
2025-04-14 22:25:58 -0400

    add new, this one is fun

---
+++ bitwise-xor-of-all-pairings/bxoap.rs
@@ -0,0 +1,27 @@
+pub fn xor_all_nums(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
+    let len1 = nums1.len();
+    let len2 = nums2.len();
+    nums1
+        .into_iter()
+        .map(|v| xor_times(v, len2))
+        .reduce(|acc, e| acc ^ e)
+        .unwrap()
+        ^ nums2
+            .into_iter()
+            .map(|v| xor_times(v, len1))
+            .reduce(|acc, e| acc ^ e)
+            .unwrap()
+}
+
+fn xor_times(v: i32, time: usize) -> i32 {
+    let mut re = v;
+    for _ in 1..time {
+        re ^= v
+    }
+
+    re
+}
+
+fn main() {
+    dbg!(xor_all_nums(vec![2, 1, 3], vec![10, 2, 5, 0]));
+}
